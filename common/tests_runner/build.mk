7633e2f4b1b72390b82cbe8c98ebe4f2e3dd362e
Maksim Ivanov <emaxx@google.com>
2016-04-14 16:27:32 +0200

    Add test runner App for running gtest tests from NaCl modules

---
+++ common/tests_runner/build.mk
@@ -0,0 +1,156 @@
+# Copyright 2016 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+#
+# Makefile for the tests runner, that is capable of executing both C++ tests
+# through the Google Test framework (including the support of NaCl-specific
+# code) and JavaScript tests (FIXME(emaxx): TBD).
+#
+# The tests are run by the "make run" command (this target is provided by the
+# NaCl SDK build scripts).
+#
+
+
+TARGET := tests_runner
+
+TESTS_RUNNER_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
+
+include $(TESTS_RUNNER_DIR)/../make/common.mk
+
+PAGE := $(OUT_DIR_PATH)/index.html
+
+include $(COMMON_DIR_PATH)/make/nacl_module_building_common.mk
+
+
+#
+# Add rules for additional dependencies that the including test may have
+# specified through the ADDITIONAL_TEST_DEPS variable (it should contain a
+# space-separated list of dependency names with, optionally, directories
+# specified after a colon).
+#
+
+ADDITIONAL_TEST_DEPS ?=
+
+TESTS_RUNNER_DEPS := \
+	ppapi_simple_cpp \
+
+EXTRACT_DEPENDENCY_NAME = $(firstword $(subst :, ,$(1)))
+
+EXTRACT_DEPENDENCY_PATH = $(word 2,$(subst :, ,$(1)))
+
+define DEPEND_RULE_WITH_PATH
+$(eval $(call DEPEND_RULE,$(call EXTRACT_DEPENDENCY_NAME,$(1)),$(call EXTRACT_DEPENDENCY_PATH,$(1))))
+endef
+
+$(foreach dep_with_dir,$(TESTS_RUNNER_DEPS) $(ADDITIONAL_TEST_DEPS),$(eval $(call DEPEND_RULE_WITH_PATH,$(dep_with_dir))))
+
+DEPS := \
+	$(TESTS_RUNNER_DEPS) \
+	$(foreach dep_with_dir,$(ADDITIONAL_TEST_DEPS),$(call EXTRACT_DEPENDENCY_NAME,$(dep_with_dir))) \
+
+
+#
+# Create a list of libraries to be linked, taking into account the additional
+# libraries that the including test may have specified through the
+# ADDITIONAL_TEST_LIBS_PREFIX and ADDITIONAL_TEST_LIBS_SUFFIX variables.
+#
+# Note that, due to PPAPI libraries internal structure, ppapi_simple_cpp must be
+# linked before ppapi and ppapi_cpp, while nacl_io must be linked after them.
+#
+
+ADDITIONAL_TEST_LIBS_PREFIX ?=
+
+ADDITIONAL_TEST_LIBS_SUFFIX ?=
+
+TESTS_RUNNER_LIBS_PREFIX := \
+	gmock \
+	gtest \
+	ppapi_simple_cpp \
+
+LIBS := \
+	$(ADDITIONAL_TEST_LIBS_PREFIX) \
+	$(TESTS_RUNNER_LIBS_PREFIX) \
+	$(DEFAULT_NACL_LIBS) \
+	$(TESTS_RUNNER_LIBS_SUFFIX) \
+	$(ADDITIONAL_TEST_LIBS_SUFFIX) \
+
+
+#
+# Create compiler flags list, taking into account the additional flags that the
+# including test may have specified through the ADDITIONAL_TEST_CPPFLAGS
+# variable.
+#
+
+CPPFLAGS := \
+	-pedantic \
+	-std=gnu++11 \
+	-Wextra \
+	-Wno-zero-length-array \
+	$(ADDITIONAL_TEST_CPPFLAGS)
+
+
+#
+# Create the list of sources.
+#
+# The including test may specify its source files through the TEST_SOURCES
+# variable.
+#
+
+TEST_SOURCES ?=
+
+TESTS_RUNNER_SOURCES_DIR := $(TESTS_RUNNER_DIR)/src
+
+TESTS_RUNNER_SOURCES := \
+	$(TESTS_RUNNER_SOURCES_DIR)/tests_runner.cc \
+
+SOURCES := \
+	$(TESTS_RUNNER_SOURCES) \
+	$(TEST_SOURCES) \
+
+
+#
+# Add the compilation rules for all sources.
+#
+
+$(foreach src,$(SOURCES),$(eval $(call COMPILE_RULE,$(src),$(CPPFLAGS))))
+
+
+#
+# Add the rules for building the NaCl module with the C++ tests.
+#
+
+$(eval $(call NACL_MODULE_BUILD_RULE,$(SOURCES),$(LIBS),$(DEPS)))
+
+
+#
+# Add the rules for copying the tests runner static files.
+#
+
+TESTS_RUNNER_STATIC_FILES := \
+	$(NACL_SDK_ROOT)/getting_started/part2/common.js \
+	$(TESTS_RUNNER_SOURCES_DIR)/index.html \
+
+$(foreach static_file,$(TESTS_RUNNER_STATIC_FILES),$(eval $(call COPY_TO_OUT_DIR_RULE,$(static_file))))
+
+
+#
+# Hack: change CURDIR to the out directory, so that the "run" target starts
+# Chrome with the correct working directory, and re-adjust path to the loaded
+# page accordingly.
+#
+
+CURDIR := $(OUT_DIR_PATH)
+
+PAGE := index.html
---
+++ common/tests_runner/src/tests_runner.cc
@@ -0,0 +1,107 @@
+// Copyright 2016 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <iostream>
+#include <string>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <ppapi/cpp/instance.h>
+#include <ppapi/cpp/var.h>
+#include <ppapi/cpp/var_dictionary.h>
+#include <ppapi_simple/ps_main.h>
+
+class GTestEventListener final : public testing::EmptyTestEventListener {
+ public:
+  void OnTestProgramStart(const testing::UnitTest& /*unit_test*/) override {
+    pp::VarDictionary message;
+    message.Set("type", "testing_started");
+    PostMessage(message);
+  }
+
+  void OnTestStart(const testing::TestInfo& test_info) override {
+    ++test_count_;
+    current_test_case_name_ = test_info.test_case_name();
+    current_test_name_ = test_info.name();
+
+    pp::VarDictionary message;
+    message.Set("type", "test_started");
+    message.Set("test_case_name", current_test_case_name_);
+    message.Set("test_name", current_test_name_);
+    PostMessage(message);
+  }
+
+  void OnTestPartResult(
+      const testing::TestPartResult& test_part_result) override {
+    if (test_part_result.failed()) {
+      pp::VarDictionary message;
+      message.Set("type", "test_part_failed");
+      message.Set("test_case_name", current_test_case_name_);
+      message.Set("test_name", current_test_name_);
+      message.Set("file_name", test_part_result.file_name());
+      message.Set("line_number", test_part_result.line_number());
+      message.Set("summary", test_part_result.summary());
+      PostMessage(message);
+    }
+  }
+
+  void OnTestEnd(const testing::TestInfo& test_info) override {
+    pp::VarDictionary message;
+    message.Set("type", "test_finished");
+    message.Set("test_case_name", current_test_case_name_);
+    message.Set("test_name", current_test_name_);
+    message.Set("ok", !test_info.result()->Failed());
+    PostMessage(message);
+
+    if (test_info.result()->Failed())
+      ++failed_test_count_;
+    current_test_case_name_.clear();
+    current_test_name_.clear();
+  }
+
+  void OnTestProgramEnd(const testing::UnitTest& /*unit_test*/) override {
+    pp::VarDictionary message;
+    message.Set("type", "testing_finished");
+    message.Set("test_count", test_count_);
+    message.Set("failed_test_count", failed_test_count_);
+    PostMessage(message);
+  }
+
+ private:
+  static void PostMessage(const pp::Var& message) {
+    pp::Instance(PSGetInstanceId()).PostMessage(message);
+  }
+
+  std::string current_test_case_name_;
+  std::string current_test_name_;
+  int test_count_ = 0;
+  int failed_test_count_ = 0;
+};
+
+int TestMain(int argc, char* argv[]) {
+  // FIXME(emaxx): For some reason, the output to stderr doesn't pass to the
+  // console when running from Chrome.
+  std::cerr.rdbuf(std::cout.rdbuf());
+
+  testing::InitGoogleTest(&argc, argv);
+  testing::InitGoogleMock(&argc, argv);
+  if (PSGetInstanceId() != 0) {
+    testing::UnitTest::GetInstance()->listeners().Append(
+        new GTestEventListener);
+  }
+  return RUN_ALL_TESTS();
+}
+
+PPAPI_SIMPLE_REGISTER_MAIN(TestMain);
