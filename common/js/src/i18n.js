8dd04b2bf42e6a16f326ac15ed2b1923d19b6904
Ivan Sandrk <isandrk@google.com>
2016-07-20 19:20:45 +0200

    Moved translation functionality to separate file, i18n.js

---
+++ common/js/src/i18n.js
@@ -0,0 +1,100 @@
+/** @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview This file contains common functionality for
+ * internationalization.
+ */
+
+
+goog.provide('GoogleSmartCard.I18n');
+
+goog.require('GoogleSmartCard.Logging');
+goog.require('goog.log.Logger');
+
+goog.scope(function() {
+
+/** @const */
+var GSC = GoogleSmartCard;
+
+/**
+ * @type {!goog.log.Logger}
+ * @const
+ */
+var logger = GSC.Logging.getScopedLogger('GoogleSmartCard.I18n');
+
+/** @const */
+var I18N_DATA_ATTRIBUTE = 'data-i18n';
+
+/** @const */
+var I18N_DATA_ARIA_LABEL_ATTRIBUTE = 'data-i18n-aria-label';
+
+/**
+ * @param {!Element} element
+ * @param {string} attribute
+ * @param {function(!Element,string)} transformFunction
+ * @private
+ */
+function transformElement(element, attribute, transformFunction) {
+  var i18nId = element.getAttribute(attribute);
+  if (!i18nId) {
+    GSC.Logging.failWithLogger(
+        logger,
+        'Failed to get element attribute: ' + element.outerHTML);
+  }
+
+  var translatedText = chrome.i18n.getMessage(i18nId);
+  if (!translatedText) {
+    GSC.Logging.failWithLogger(
+        logger,
+        'Failed to get translation for text with id: ' + i18nId);
+  }
+
+  transformFunction(element, translatedText);
+}
+
+/**
+ * @param {string} attribute
+ * @param {function(!Element,string)} transformFunction
+ * @private
+ */
+function transformAllElements(attribute, transformFunction) {
+  var selector = '[' + attribute + ']';
+  for (let element of document.querySelectorAll(selector)) {
+    transformElement(element, attribute, transformFunction);
+  }
+}
+
+/**
+ * Goes through all the HTML elements in the current window, replacing
+ * element.textContent with translation for elements containing attribute
+ * I18N_DATA_ATTRIBUTE, and setting attribute aria-label to translation for
+ * elements containing attribute I18N_DATA_ARIA_LABEL_ATTRIBUTE. Used for
+ * translation that plays nice with Chromevox (accessibility tool).
+ */
+GSC.I18n.adjustElementsTranslation = function() {
+  transformAllElements(
+      I18N_DATA_ATTRIBUTE, function(element, translatedText) {
+    element.textContent = translatedText;
+  });
+
+  transformAllElements(
+      I18N_DATA_ARIA_LABEL_ATTRIBUTE, function(element, translatedText) {
+    element.setAttribute('aria-label', translatedText);
+  });
+}
+
+});  // goog.scope
--- smart_card_connector_app/src/window-main.js
+++ smart_card_connector_app/src/window-main.js
@@ -24,6 +24,7 @@ goog.require('GoogleSmartCard.ConnectorApp.Window.AboutShowing');
 goog.require('GoogleSmartCard.ConnectorApp.Window.AppsDisplaying');
 goog.require('GoogleSmartCard.ConnectorApp.Window.DevicesDisplaying');
 goog.require('GoogleSmartCard.ConnectorApp.Window.LogsExporting');
+goog.require('GoogleSmartCard.I18n');
 goog.require('GoogleSmartCard.Logging');
 goog.require('GoogleSmartCard.PopupWindow.Client');
 goog.require('goog.dom');
@@ -60,52 +61,7 @@ GSC.ConnectorApp.Window.LogsExporting.initialize();
 
 GSC.PopupWindow.Client.showWindow();
 
-/** @const */
-var I18N_DATA_ATTRIBUTE = 'data-i18n';
-
-/** @const */
-var I18N_DATA_ARIA_LABEL_ATTRIBUTE = 'data-i18n-aria-label';
-
-function setElementI18nText(element) {
-  var i18nId = element.getAttribute(I18N_DATA_ATTRIBUTE);
-  if (!i18nId) {
-    GSC.Logging.failWithLogger(
-        logger,
-        'Failed to get element attribute: ' + element.outerHTML);
-  }
-  var translatedText = chrome.i18n.getMessage(i18nId);
-  if (!translatedText) {
-    GSC.Logging.failWithLogger(
-        logger,
-        'Failed to get translation for text with id: ' + i18nId);
-  }
-  element.textContent = translatedText;
-}
-
-function setElementI18nAriaLabel(element) {
-  var i18nId = element.getAttribute(I18N_DATA_ARIA_LABEL_ATTRIBUTE);
-  if (!i18nId) {
-    GSC.Logging.failWithLogger(
-        logger,
-        'Failed to get element attribute: ' + element.outerHTML);
-  }
-  var translatedText = chrome.i18n.getMessage(i18nId);
-  if (!translatedText) {
-    GSC.Logging.failWithLogger(
-        logger,
-        'Failed to get translation for text with id: ' + i18nId);
-  }
-  element.setAttribute('aria-label', translatedText);
-}
-
-for (let element of document.querySelectorAll(
-    '[' + I18N_DATA_ATTRIBUTE + ']')) {
-  setElementI18nText(element);
-}
-
-for (let element of document.querySelectorAll(
-    '[' + I18N_DATA_ARIA_LABEL_ATTRIBUTE + ']')) {
-  setElementI18nAriaLabel(element);
-}
+// This call does all of the translation work.
+GSC.I18n.adjustElementsTranslation();
 
 });  // goog.scope
