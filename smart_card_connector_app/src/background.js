45b1a1e45943c8b6692b5b9fa97238d1e0364a48
Maksim Ivanov <emaxx@google.com>
2019-01-03 13:10:12 +0100

    Log current time in Connector app

--- smart_card_connector_app/src/background.js
+++ smart_card_connector_app/src/background.js
@@ -42,8 +42,9 @@ var logger = GSC.Logging.getScopedLogger('ConnectorApp.BackgroundMain');
 
 logger.info(
     'The Smart Card Connector app (id "' + chrome.runtime.id + '", version ' +
-    chrome.runtime.getManifest()['version'] + ') background script started');
-logger.info('Browser version: "' + window.navigator.appVersion + '"');
+    chrome.runtime.getManifest()['version'] + ') background script started. ' +
+    'Browser version: "' + window.navigator.appVersion + '". System time: "' +
+    (new Date()).toLocaleString() + '"');
 
 var naclModule = new GSC.NaclModule(
     'nacl_module.nmf', GSC.NaclModule.Type.PNACL);
