ec3c7772f139aece4f1596c26a3cb07510cb666b
Maksim Ivanov <emaxx@google.com>
2020-10-06 03:32:02 +0200

    Smoke tests for setCertificates() C++ adapter
    Add first integration tests that verify the triggered JavaScript
    Extensions API calls after running the helper C++ function
    chrome_certificate_provider::ApiBridge::SetCertificates().
    
    These first tests cover only the simplest scenario: when the function is
    called with an empty list of certificates. The tests, however, cover
    both cases of the up-to-date and the legacy chrome.certificateProvider
    APIs.

---
+++ example_cpp_smart_card_client_app/src/chrome_certificate_provider/api_bridge_integration_test_helper.cc
@@ -0,0 +1,129 @@
+// Copyright 2020 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include <ppapi/cpp/core.h>
+#include <ppapi/cpp/instance.h>
+#include <ppapi/cpp/var.h>
+
+#include <google_smart_card_common/logging/logging.h>
+#include <google_smart_card_common/messaging/typed_message_router.h>
+#include <google_smart_card_common/pp_var_utils/debug_dump.h>
+#include <google_smart_card_common/pp_var_utils/extraction.h>
+#include <google_smart_card_common/requesting/request_receiver.h>
+#include <google_smart_card_common/requesting/request_result.h>
+#include <google_smart_card_common/unique_ptr_utils.h>
+#include <google_smart_card_integration_testing/integration_test_helper.h>
+#include <google_smart_card_integration_testing/integration_test_service.h>
+
+#include "chrome_certificate_provider/api_bridge.h"
+
+namespace gsc = google_smart_card;
+
+namespace smart_card_client {
+namespace chrome_certificate_provider {
+
+namespace {
+
+void SetCertificatesOnBackgroundThread(
+    std::weak_ptr<ApiBridge> api_bridge,
+    const std::vector<ClientCertificateInfo>& certificates,
+    gsc::RequestReceiver::ResultCallback result_callback) {
+  std::shared_ptr<ApiBridge> locked_api_bridge = api_bridge.lock();
+  if (!locked_api_bridge)
+    GOOGLE_SMART_CARD_LOG_FATAL << "ApiBridge already destroyed";
+  locked_api_bridge->SetCertificates(certificates);
+  result_callback(gsc::GenericRequestResult::CreateSuccessful(pp::Var()));
+}
+
+}  // namespace
+
+class ApiBridgeIntegrationTestHelper final : public gsc::IntegrationTestHelper {
+ public:
+  // IntegrationTestHelper:
+  std::string GetName() const override;
+  void SetUp(
+      pp::Instance* pp_instance,
+      pp::Core* pp_core,
+      gsc::TypedMessageRouter* typed_message_router,
+      const pp::Var& data) override;
+  void TearDown() override;
+  void OnMessageFromJs(
+      const pp::Var& data,
+      gsc::RequestReceiver::ResultCallback result_callback) override;
+
+ private:
+  void ScheduleSetCertificatesCall(
+      const std::vector<ClientCertificateInfo>& certificates,
+      gsc::RequestReceiver::ResultCallback result_callback);
+
+  std::shared_ptr<ApiBridge> api_bridge_;
+};
+
+const auto g_foo_helper = gsc::IntegrationTestService::RegisterHelper(
+    gsc::MakeUnique<ApiBridgeIntegrationTestHelper>());
+
+std::string ApiBridgeIntegrationTestHelper::GetName() const {
+  return "ChromeCertificateProviderApiBridge";
+}
+
+void ApiBridgeIntegrationTestHelper::SetUp(
+    pp::Instance* pp_instance,
+    pp::Core* pp_core,
+    gsc::TypedMessageRouter* typed_message_router,
+    const pp::Var& /*data*/) {
+  api_bridge_ = std::make_shared<ApiBridge>(
+      typed_message_router,
+      pp_instance,
+      pp_core,
+      /*request_handling_mutex=*/nullptr);
+}
+
+void ApiBridgeIntegrationTestHelper::TearDown() {
+  api_bridge_->Detach();
+  api_bridge_.reset();
+}
+
+void ApiBridgeIntegrationTestHelper::OnMessageFromJs(
+    const pp::Var& data,
+    gsc::RequestReceiver::ResultCallback result_callback) {
+  std::string command;
+  std::string error_message;
+  if (!gsc::VarAs(data, &command, &error_message))
+    GOOGLE_SMART_CARD_LOG_FATAL << "Unexpected message " << gsc::DumpVar(data);
+  if (command == "setCertificates_empty") {
+    ScheduleSetCertificatesCall(/*certificates=*/{}, result_callback);
+  } else {
+    GOOGLE_SMART_CARD_LOG_FATAL << "Unknown command " << command;
+  }
+}
+
+void ApiBridgeIntegrationTestHelper::ScheduleSetCertificatesCall(
+    const std::vector<ClientCertificateInfo>& certificates,
+    gsc::RequestReceiver::ResultCallback result_callback) {
+  // Post to a background thread, since the main thread isn't allowed to perform
+  // blocking calls, which SetCertificates() is.
+  std::thread(
+      &SetCertificatesOnBackgroundThread,
+      api_bridge_,
+      certificates,
+      result_callback).detach();
+}
+
+}  // namespace chrome_certificate_provider
+}  // namespace smart_card_client
---
+++ example_cpp_smart_card_client_app/src/chrome_certificate_provider/bridge-integrationtest.js
@@ -0,0 +1,104 @@
+/** @license
+ * Copyright 2020 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+goog.require('SmartCardClientApp.CertificateProviderBridge.Backend');
+goog.require('GoogleSmartCard.IntegrationTestController');
+goog.require('GoogleSmartCard.Logging');
+goog.require('goog.Promise');
+goog.require('goog.promise.Resolver');
+goog.require('goog.testing');
+goog.require('goog.testing.jsunit');
+
+goog.setTestOnly();
+
+goog.scope(function() {
+
+const GSC = GoogleSmartCard;
+const CertificateProviderBridge = SmartCardClientApp.CertificateProviderBridge;
+
+/** @type {GSC.IntegrationTestController?} */
+let testController;
+/** @type {CertificateProviderBridge.Backend?} */
+let bridgeBackend;
+/** @type {!goog.promise.Resolver<!chrome.certificateProvider.SetCertificatesDetails>} */
+let setCertificatesApiExpectation;
+
+/**
+ * Sets up stub implementation of the chrome.certificateProvider API.
+ */
+function setUpApiStubs() {
+  testController.propertyReplacer.set(chrome, 'certificateProvider', {
+    'setCertificates': stubSetCertificatesApi,
+  });
+}
+
+/**
+ * Sets up stub implementation of the legacy chrome.certificateProvider API (the
+ * one that existed before the setCertificates() method was introduced).
+ */
+function setUpLegacyApiStubs() {
+  testController.propertyReplacer.set(chrome, 'certificateProvider', {});
+}
+
+/**
+ * Stub implementation of the chrome.certificateProvider.setCertificates() API.
+ * @param {!chrome.certificateProvider.SetCertificatesDetails} details
+ * @param {function()=} callback
+ */
+function stubSetCertificatesApi(details, callback) {
+  setCertificatesApiExpectation.resolve(details);
+  if (callback)
+    callback();
+}
+
+goog.exportSymbol('testChromeCertificateProviderApiBridge', {
+  setUp: function() {
+    testController = new GSC.IntegrationTestController();
+    setCertificatesApiExpectation = goog.Promise.withResolver();
+    return testController.initAsync().then(() => {
+      bridgeBackend = new CertificateProviderBridge.Backend(
+          testController.naclModule);
+      return testController.setUpCppHelper(
+        'ChromeCertificateProviderApiBridge', /*helperArgument=*/{});
+    });
+  },
+
+  tearDown: function() {
+    return testController.disposeAsync().thenAlways(() => {
+      testController = null;
+    });
+  },
+
+  testSetCertificates_empty: function() {
+    setUpApiStubs();
+    testController.sendMessageToCppHelper(
+        'ChromeCertificateProviderApiBridge',
+        /*messageForHelper=*/'setCertificates_empty');
+    return setCertificatesApiExpectation.promise.then((details) => {
+      assertObjectEquals(details, {'clientCertificates': []});
+    });
+  },
+
+  testSetCertificates_empty_legacyApi: function() {
+    setUpLegacyApiStubs();
+    // Just verify that no crash happens.
+    return testController.sendMessageToCppHelper(
+        'ChromeCertificateProviderApiBridge',
+        /*messageForHelper=*/'setCertificates_empty');
+  },
+});
+
+});  // goog.scope
