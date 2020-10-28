bfce949b35e0a412710127e52678e7a7bf1760a1
Maksim Ivanov <emaxx@google.com>
2020-10-28 11:39:51 +0100

    Build //common NaCl-specific files only under NaCl
    This fixes the makefiles in //common/cpp to exclude source files from
    compilation in case the toolchain is non Native Client and the files
    can only be compiled under the Native Client.
    
    This fixes the Emscripten/WebAssembly builds of //common/cpp (as tracked
    by #177). Note that there are two groups of excluded files currently:
    those that provide NaCl-specific capabilities, and those that aren't
    migrated to support Emscripten yet. The latter ones will be gradually
    migrated and reenabled in follow-ups.

--- common/cpp/src/google_smart_card_common/pp_var_utils/debug_dump.h
+++ common/cpp/src/google_smart_card_common/pp_var_utils/debug_dump.h
@@ -18,6 +18,10 @@
 #ifndef GOOGLE_SMART_CARD_COMMON_PP_VAR_UTILS_DEBUG_DUMP_H_
 #define GOOGLE_SMART_CARD_COMMON_PP_VAR_UTILS_DEBUG_DUMP_H_
 
+#ifndef __native_client__
+#error "This file should only be used in Native Client builds"
+#endif  // __native_client__
+
 #include <string>
 
 #include <ppapi/cpp/var.h>
