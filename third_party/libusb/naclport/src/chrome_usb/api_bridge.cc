9db73bb9334827b6e481e0702a8d3368cf5f144f
Maksim Ivanov <emaxx@google.com>
2020-11-06 15:26:54 +0100

    Migrate remote_call_adaptor.{h|cc} to Value (#238)
    This commit changes the RemoteCallAdaptor class to use the
    toolchain-independent Value class instead of Native Client specific
    classes (like pp::Var). This contributes to the effort tracked
    by #185.
    
    Besides mechanical replacements, this change introduces a helper class
    for converting the input/output remote call arguments from Values into
    any supported C++ type. Unlike old NaCl-based conversion helpers,
    this class is more flexible (e.g., in case the argument list doesn't have a
    fixed length) and produces better error messages.
    
    As a side effect bonus, this commit made the following files
    toolchain-independent as well:
    * chrome_usb/api_bridge.cc;
    * pcsc_lite_over_requester.cc.

--- third_party/libusb/naclport/src/chrome_usb/api_bridge.cc
+++ third_party/libusb/naclport/src/chrome_usb/api_bridge.cc
@@ -38,97 +38,94 @@ void ApiBridge::Detach() {
 
 RequestResult<GetDevicesResult> ApiBridge::GetDevices(
     const GetDevicesOptions& options) {
-  const GenericRequestResult generic_request_result =
+  GenericRequestResult generic_request_result =
       remote_call_adaptor_.SyncCall("getDevices", options);
   GetDevicesResult result;
-  return RemoteCallAdaptor::ConvertResultPayload(generic_request_result,
-                                                 &result, &result.devices);
+  return RemoteCallAdaptor::ConvertResultPayload(
+      std::move(generic_request_result), &result, &result.devices);
 }
 
 RequestResult<GetUserSelectedDevicesResult> ApiBridge::GetUserSelectedDevices(
     const GetUserSelectedDevicesOptions& options) {
-  const GenericRequestResult generic_request_result =
+  GenericRequestResult generic_request_result =
       remote_call_adaptor_.SyncCall("getUserSelectedDevices", options);
   GetUserSelectedDevicesResult result;
-  return RemoteCallAdaptor::ConvertResultPayload(generic_request_result,
-                                                 &result, &result.devices);
+  return RemoteCallAdaptor::ConvertResultPayload(
+      std::move(generic_request_result), &result, &result.devices);
 }
 
 RequestResult<GetConfigurationsResult> ApiBridge::GetConfigurations(
     const Device& device) {
-  const GenericRequestResult generic_request_result =
+  GenericRequestResult generic_request_result =
       remote_call_adaptor_.SyncCall("getConfigurations", device);
   GetConfigurationsResult result;
   return RemoteCallAdaptor::ConvertResultPayload(
-      generic_request_result, &result, &result.configurations);
+      std::move(generic_request_result), &result, &result.configurations);
 }
 
 RequestResult<OpenDeviceResult> ApiBridge::OpenDevice(const Device& device) {
-  const GenericRequestResult generic_request_result =
+  GenericRequestResult generic_request_result =
       remote_call_adaptor_.SyncCall("openDevice", device);
   OpenDeviceResult result;
   return RemoteCallAdaptor::ConvertResultPayload(
-      generic_request_result, &result, &result.connection_handle);
+      std::move(generic_request_result), &result, &result.connection_handle);
 }
 
 RequestResult<CloseDeviceResult> ApiBridge::CloseDevice(
     const ConnectionHandle& connection_handle) {
-  const GenericRequestResult generic_request_result =
+  GenericRequestResult generic_request_result =
       remote_call_adaptor_.SyncCall("closeDevice", connection_handle);
   CloseDeviceResult result;
-  return RemoteCallAdaptor::ConvertResultPayload(generic_request_result,
-                                                 &result);
+  return RemoteCallAdaptor::ConvertResultPayload(
+      std::move(generic_request_result), &result);
 }
 
 RequestResult<SetConfigurationResult> ApiBridge::SetConfiguration(
     const ConnectionHandle& connection_handle,
     int64_t configuration_value) {
-  const GenericRequestResult generic_request_result =
-      remote_call_adaptor_.SyncCall("setConfiguration", connection_handle,
-                                    configuration_value);
+  GenericRequestResult generic_request_result = remote_call_adaptor_.SyncCall(
+      "setConfiguration", connection_handle, configuration_value);
   SetConfigurationResult result;
-  return RemoteCallAdaptor::ConvertResultPayload(generic_request_result,
-                                                 &result);
+  return RemoteCallAdaptor::ConvertResultPayload(
+      std::move(generic_request_result), &result);
 }
 
 RequestResult<GetConfigurationResult> ApiBridge::GetConfiguration(
     const ConnectionHandle& connection_handle) {
-  const GenericRequestResult generic_request_result =
+  GenericRequestResult generic_request_result =
       remote_call_adaptor_.SyncCall("getConfiguration", connection_handle);
   GetConfigurationResult result;
   return RemoteCallAdaptor::ConvertResultPayload(
-      generic_request_result, &result, &result.configuration);
+      std::move(generic_request_result), &result, &result.configuration);
 }
 
 RequestResult<ListInterfacesResult> ApiBridge::ListInterfaces(
     const ConnectionHandle& connection_handle) {
-  const GenericRequestResult generic_request_result =
+  GenericRequestResult generic_request_result =
       remote_call_adaptor_.SyncCall("listInterfaces", connection_handle);
   ListInterfacesResult result;
-  return RemoteCallAdaptor::ConvertResultPayload(generic_request_result,
-                                                 &result, &result.descriptors);
+  return RemoteCallAdaptor::ConvertResultPayload(
+      std::move(generic_request_result), &result, &result.descriptors);
 }
 
 RequestResult<ClaimInterfaceResult> ApiBridge::ClaimInterface(
     const ConnectionHandle& connection_handle,
     int64_t interface_number) {
-  const GenericRequestResult generic_request_result =
-      remote_call_adaptor_.SyncCall("claimInterface", connection_handle,
-                                    interface_number);
+  GenericRequestResult generic_request_result = remote_call_adaptor_.SyncCall(
+      "claimInterface", connection_handle, interface_number);
   ClaimInterfaceResult result;
-  return RemoteCallAdaptor::ConvertResultPayload(generic_request_result,
-                                                 &result);
+  return RemoteCallAdaptor::ConvertResultPayload(
+      std::move(generic_request_result), &result);
 }
 
 RequestResult<ReleaseInterfaceResult> ApiBridge::ReleaseInterface(
     const ConnectionHandle& connection_handle,
     int64_t interface_number) {
-  const GenericRequestResult generic_request_result =
-      remote_call_adaptor_.SyncCall("releaseInterface", connection_handle,
-                                    interface_number);
+  GenericRequestResult generic_request_result = remote_call_adaptor_.SyncCall(
+      "releaseInterface", connection_handle, interface_number);
   ReleaseInterfaceResult result;
-  return RemoteCallAdaptor::ConvertResultPayload(generic_request_result,
-                                                 &result);
+  return RemoteCallAdaptor::ConvertResultPayload(
+      std::move(generic_request_result), &result);
 }
 
 namespace {
@@ -138,7 +135,7 @@ GenericAsyncRequestCallback WrapAsyncTransferCallback(
   return [callback](GenericRequestResult generic_request_result) {
     TransferResult result;
     return callback(RemoteCallAdaptor::ConvertResultPayload(
-        generic_request_result, &result, &result.result_info));
+        std::move(generic_request_result), &result, &result.result_info));
   };
 }
 
@@ -171,11 +168,11 @@ void ApiBridge::AsyncInterruptTransfer(
 
 RequestResult<ResetDeviceResult> ApiBridge::ResetDevice(
     const ConnectionHandle& connection_handle) {
-  const GenericRequestResult generic_request_result =
+  GenericRequestResult generic_request_result =
       remote_call_adaptor_.SyncCall("resetDevice", connection_handle);
   ResetDeviceResult result;
   return RemoteCallAdaptor::ConvertResultPayload(
-      generic_request_result, &result, &result.reset_success);
+      std::move(generic_request_result), &result, &result.reset_success);
 }
 
 }  // namespace chrome_usb
