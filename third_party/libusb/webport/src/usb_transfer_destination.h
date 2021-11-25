f5f282adae4d4d704ea628dd300d35f3e3136a67
Maksim Ivanov <emaxx@google.com>
2021-11-25 14:06:44 +0100

    [usb] Decouple USB destination from chrome.usb (#455)
    Refactor the UsbTransferDestination struct to stop using the chrome.usb
    entities internally, and also to expose factory methods that don't use
    chrome.usb entities.
    
    This is a preparation for switching the callsites away from using
    chrome.usb. It's preparatory work for the WebUSB support effort tracked
    by #429.

--- third_party/libusb/webport/src/usb_transfer_destination.h
+++ third_party/libusb/webport/src/usb_transfer_destination.h
@@ -19,6 +19,8 @@
 
 #include <stdint.h>
 
+#include <libusb.h>
+
 #include <google_smart_card_common/optional.h>
 
 #include "chrome_usb/types.h"
@@ -28,7 +30,7 @@ namespace google_smart_card {
 // This structure uniquely represents a libusb/chrome.usb transfer destination.
 //
 // This structure is used for finding matches between transfers and transfer
-// results (see the comments in the libusb_over_chrome_usb.h header).
+// results (see the comments in the libusb_js_proxy.h header).
 class UsbTransferDestination final {
  public:
   UsbTransferDestination();
@@ -36,10 +38,19 @@ class UsbTransferDestination final {
   UsbTransferDestination& operator=(const UsbTransferDestination&) = default;
   ~UsbTransferDestination();
 
+  static UsbTransferDestination CreateForControlTransfer(
+      int64_t js_device_handle,
+      uint8_t request_type,
+      uint8_t request,
+      uint16_t value,
+      uint16_t index);
+  static UsbTransferDestination CreateForGenericTransfer(
+      int64_t js_device_handle,
+      uint8_t endpoint_address);
+
   static UsbTransferDestination CreateFromChromeUsbControlTransfer(
       const chrome_usb::ConnectionHandle& connection_handle,
       const chrome_usb::ControlTransferInfo& transfer_info);
-
   static UsbTransferDestination CreateFromChromeUsbGenericTransfer(
       const chrome_usb::ConnectionHandle& connection_handle,
       const chrome_usb::GenericTransferInfo& transfer_info);
@@ -51,29 +62,21 @@ class UsbTransferDestination final {
   bool operator==(const UsbTransferDestination& other) const;
 
  private:
-  UsbTransferDestination(const chrome_usb::ConnectionHandle& connection_handle,
-                         const chrome_usb::Direction& direction,
-                         optional<int64_t> endpoint,
-                         optional<chrome_usb::ControlTransferInfoRecipient>
-                             control_transfer_recipient,
-                         optional<chrome_usb::ControlTransferInfoRequestType>
-                             control_transfer_request_type,
-                         optional<int64_t> control_transfer_request,
-                         optional<int64_t> control_transfer_value,
-                         optional<int64_t> control_transfer_index);
+  UsbTransferDestination(int64_t js_device_handle,
+                         optional<uint8_t> endpoint_address,
+                         optional<uint8_t> control_transfer_request_type,
+                         optional<uint8_t> control_transfer_request,
+                         optional<uint16_t> control_transfer_value,
+                         optional<uint16_t> control_transfer_index);
 
   int Compare(const UsbTransferDestination& other) const;
 
-  chrome_usb::ConnectionHandle connection_handle_;
-  chrome_usb::Direction direction_;
-  optional<int64_t> endpoint_;
-  optional<chrome_usb::ControlTransferInfoRecipient>
-      control_transfer_recipient_;
-  optional<chrome_usb::ControlTransferInfoRequestType>
-      control_transfer_request_type_;
-  optional<int64_t> control_transfer_request_;
-  optional<int64_t> control_transfer_value_;
-  optional<int64_t> control_transfer_index_;
+  int64_t js_device_handle_;
+  optional<uint8_t> endpoint_address_;
+  optional<uint8_t> control_transfer_request_type_;
+  optional<uint8_t> control_transfer_request_;
+  optional<uint16_t> control_transfer_value_;
+  optional<uint16_t> control_transfer_index_;
 };
 
 }  // namespace google_smart_card
