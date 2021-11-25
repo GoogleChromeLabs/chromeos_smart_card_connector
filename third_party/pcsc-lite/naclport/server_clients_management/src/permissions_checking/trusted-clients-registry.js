GoogleChromeLabs/chromeos_smart_card_connector

commit e697673f2d172a39df315cb953655797d637c995

Switch known_client_apps.json to origins (#412)
Switch the JSON config that describes all client applications that are
"trusted" to list origins instead of extension IDs. This change is part
of the general plan to switch to origins, allowing us to support
requests from web pages in the same way as how we supported them from
extensions.

Also rename a few source files and classes to better reflect the new
function: "KnownApp" => "TrustedClientInfo", "KnownAppsRegistry" =>
"TrustedClientsRegistry". We don't rename the JSON config since it'll
break github links in various old docs and conversations.

This is a preparatory commit for the effort tracked by #377: supporting
PC/SC requests from web pages.

third_party/pcsc-lite/naclport/server_clients_management/src/permissions_checking/trusted-clients-registry.js
@@ -26,8 +26,8 @@
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-goog.provide('GoogleSmartCard.PcscLiteServerClientsManagement.PermissionsChecking.KnownApp');
-goog.provide('GoogleSmartCard.PcscLiteServerClientsManagement.PermissionsChecking.KnownAppsRegistry');
+goog.provide('GoogleSmartCard.PcscLiteServer.TrustedClientInfo');
+goog.provide('GoogleSmartCard.PcscLiteServer.TrustedClientsRegistry');
 
 goog.require('GoogleSmartCard.DebugDump');
 goog.require('GoogleSmartCard.Json');
@@ -42,76 +42,76 @@ goog.require('goog.promise.Resolver');
 
 goog.scope(function() {
 
-const KNOWN_CLIENT_APPS_JSON_URL =
+// The actual data is loaded from this JSON config.
+const JSON_CONFIG_URL =
     'pcsc_lite_server_clients_management/known_client_apps.json';
 
-const KNOWN_CLIENT_APP_NAME_FIELD = 'name';
+const CLIENT_NAME_FIELD = 'name';
 
 const GSC = GoogleSmartCard;
 
-const PermissionsChecking =
-    GSC.PcscLiteServerClientsManagement.PermissionsChecking;
-
 /**
- * This structure holds the information about the known client app.
- * @param {string} id
+ * This structure holds the information about a trusted client.
+ * @param {string} origin
  * @param {string} name
  * @constructor
  */
-PermissionsChecking.KnownApp = function(id, name) {
+GSC.PcscLiteServer.TrustedClientInfo = function(origin, name) {
   /** @const */
-  this.id = id;
+  this.origin = origin;
   /** @const */
   this.name = name;
 };
 
-const KnownApp = PermissionsChecking.KnownApp;
+const TrustedClientInfo = GSC.PcscLiteServer.TrustedClientInfo;
 
 /**
- * This class provides an interface for querying the information about the known
- * client apps (i.e., the apps that are listed in the manually maintained
+ * This class provides an interface for querying the information about trusted
+ * client applications (i.e., the clients that are listed in the manually
+ * maintained
  * //third_party/pcsc-lite/naclport/server_clients_management/src/known_client_apps.json
  * config).
  * @constructor
  */
-PermissionsChecking.KnownAppsRegistry = function() {
+GSC.PcscLiteServer.TrustedClientsRegistry = function() {
   /**
-   * @type {!goog.promise.Resolver.<!Map.<string,!KnownApp>>} @private @const
+   * @type {!goog.promise.Resolver.<!Map.<string,!TrustedClientInfo>>} @private @const
    */
   this.promiseResolver_ = goog.Promise.withResolver();
 
   this.startLoadingJson_();
 };
 
-const KnownAppsRegistry = PermissionsChecking.KnownAppsRegistry;
+const TrustedClientsRegistry = GSC.PcscLiteServer.TrustedClientsRegistry;
 
 /**
  * @type {!goog.log.Logger}
  * @const
  */
-KnownAppsRegistry.prototype.logger = GSC.Logging.getScopedLogger(
-    'PcscLiteServerClientsManagement.PermissionsChecking.KnownAppsRegistry');
+TrustedClientsRegistry.prototype.logger =
+    GSC.Logging.getScopedLogger('PcscLiteServer.TrustedClientsRegistry');
 
 /**
- * Requests information about the given app from the config that contains the
- * list of known client apps.
+ * Requests information about the given client from the config that contains the
+ * list of known clients.
  *
  * The result is returned asynchronously as a promise (which will be rejected if
  * the given app isn't present in the config).
- * @param {string} id
- * @return {!goog.Promise.<!KnownApp>}
+ * @param {string} origin
+ * @return {!goog.Promise.<!TrustedClientInfo>}
  */
-KnownAppsRegistry.prototype.getById = function(id) {
+TrustedClientsRegistry.prototype.getByOrigin = function(origin) {
   const promiseResolver = goog.Promise.withResolver();
 
-  this.tryGetByIds([id]).then(
-      function(knownApps) {
-        const knownApp = knownApps[0];
-        if (knownApp) {
-          promiseResolver.resolve(knownApp);
+  this.tryGetByOrigins([origin]).then(
+      function(infos) {
+        GSC.Logging.checkWithLogger(this.logger, infos.length === 1);
+        const info = infos[0];
+        if (info) {
+          promiseResolver.resolve(info);
         } else {
           promiseResolver.reject(new Error(
-              'The specified App id is not in the known Apps registry'));
+              'The specified origin is not in the trusted clients registry'));
         }
       },
       function(error) {
@@ -122,58 +122,56 @@ KnownAppsRegistry.prototype.getById = function(id) {
 };
 
 /**
- * Similar to getById(), but performs a batch request for the list of given IDs
- * at once.
+ * Similar to getByOrigin(), but performs a batch request for the list of given
+ * origins at once.
  *
  * The result is returned asynchronously as a promise that, when resolved, will
- * contain the same number of elements as |idList|, with the i-th value
- * containing either the information for the i-th app in |idList| or |null| if
- * the app isn't present in the config.
- * @param {!Array.<string>} idList
- * @return {!goog.Promise.<!Array.<?KnownApp>>}
+ * contain the same number of elements as |originList|, with the i-th value
+ * containing either the information for the i-th client in |originList| or
+ * |null| if the client isn't present in the config.
+ * @param {!Array.<string>} originList
+ * @return {!goog.Promise.<!Array.<?TrustedClientInfo>>}
  */
-KnownAppsRegistry.prototype.tryGetByIds = function(idList) {
+TrustedClientsRegistry.prototype.tryGetByOrigins = function(originList) {
   const promiseResolver = goog.Promise.withResolver();
 
   this.promiseResolver_.promise.then(
-      function(knownAppsMap) {
-        const knownApps = [];
-        for (let id of idList) {
-          const knownApp = knownAppsMap.get(id);
-          knownApps.push(knownApp !== undefined ? knownApp : null);
+      function(originToInfoMap) {
+        const infos = [];
+        for (const origin of originList) {
+          const info = originToInfoMap.get(origin);
+          infos.push(info !== undefined ? info : null);
         }
-        promiseResolver.resolve(knownApps);
+        promiseResolver.resolve(infos);
       },
       function() {
         promiseResolver.reject(
-            new Error('Failed to load the known Apps registry'));
+            new Error('Failed to load the trusted clients registry'));
       });
 
   return promiseResolver.promise;
 };
 
 /** @private */
-KnownAppsRegistry.prototype.startLoadingJson_ = function() {
+TrustedClientsRegistry.prototype.startLoadingJson_ = function() {
   goog.log.fine(
       this.logger,
-      'Loading registry from JSON file (URL: "' + KNOWN_CLIENT_APPS_JSON_URL +
-          '")...');
-  goog.net.XhrIo.send(
-      KNOWN_CLIENT_APPS_JSON_URL, this.jsonLoadedCallback_.bind(this));
+      'Loading registry from JSON file (URL: "' + JSON_CONFIG_URL + '")...');
+  goog.net.XhrIo.send(JSON_CONFIG_URL, this.jsonLoadedCallback_.bind(this));
 };
 
 /** @private */
-KnownAppsRegistry.prototype.jsonLoadedCallback_ = function(e) {
+TrustedClientsRegistry.prototype.jsonLoadedCallback_ = function(e) {
   /** @type {!goog.net.XhrIo} */
   const xhrio = e.target;
 
   if (!xhrio.isSuccess()) {
     this.promiseResolver_.reject(
-        new Error('Failed to load the known Apps registry'));
+        new Error('Failed to load the trusted clients registry'));
     GSC.Logging.failWithLogger(
         this.logger,
-        'Failed to load the known Apps registry from JSON file (URL: "' +
-            KNOWN_CLIENT_APPS_JSON_URL +
+        'Failed to load the trusted clients registry from JSON file (URL: "' +
+            JSON_CONFIG_URL +
             '") with the following error: ' + xhrio.getLastError());
   }
 
@@ -188,12 +186,11 @@ KnownAppsRegistry.prototype.jsonLoadedCallback_ = function(e) {
           },
           function(exc) {
             this.promiseResolver_.reject(
-                new Error('Failed to load the known Apps registry'));
+                new Error('Failed to load the trusted clients registry'));
             GSC.Logging.failWithLogger(
                 this.logger,
-                'Failed to load the known Apps registry from JSON file (URL: "' +
-                    KNOWN_CLIENT_APPS_JSON_URL +
-                    '"): parsing failed with the ' +
+                'Failed to load the trusted clients registry from JSON file (URL: "' +
+                    JSON_CONFIG_URL + '"): parsing failed with the ' +
                     'following error: ' + exc);
           },
           this);
@@ -203,18 +200,18 @@ KnownAppsRegistry.prototype.jsonLoadedCallback_ = function(e) {
  * @param {!Object} json
  * @private
  */
-KnownAppsRegistry.prototype.parseJsonAndApply_ = function(json) {
-  /** @type {!Map.<string, !KnownApp>} */
-  const knownClientApps = new Map;
+TrustedClientsRegistry.prototype.parseJsonAndApply_ = function(json) {
+  /** @type {!Map.<string, !TrustedClientInfo>} */
+  const originToInfoMap = new Map;
   let success = true;
   goog.object.forEach(json, function(value, key) {
-    const knownApp = this.tryParseKnownAppJson_(key, value);
-    if (knownApp) {
-      knownClientApps.set(knownApp.id, knownApp);
+    const info = this.tryParseTrustedClientInfo_(key, value);
+    if (info) {
+      originToInfoMap.set(info.origin, info);
     } else {
       goog.log.warning(
           this.logger,
-          'Failed to parse the following known Apps registry JSON item: key="' +
+          'Failed to parse the following trusted clients registry JSON item: key="' +
               key + '", value=' + GSC.DebugDump.dump(value));
       success = false;
     }
@@ -224,15 +221,15 @@ KnownAppsRegistry.prototype.parseJsonAndApply_ = function(json) {
     goog.log.fine(
         this.logger,
         'Successfully loaded registry from JSON file: ' +
-            GSC.DebugDump.dump(knownClientApps));
-    this.promiseResolver_.resolve(knownClientApps);
+            GSC.DebugDump.dump(originToInfoMap));
+    this.promiseResolver_.resolve(originToInfoMap);
   } else {
     this.promiseResolver_.reject(
-        new Error('Failed to load the known Apps registry'));
+        new Error('Failed to load the trusted clients registry'));
     GSC.Logging.failWithLogger(
         this.logger,
-        'Failed to load the known Apps registry from JSON file (URL: "' +
-            KNOWN_CLIENT_APPS_JSON_URL + '"): parsing of some of the items ' +
+        'Failed to load the trusted clients registry from JSON file (URL: "' +
+            JSON_CONFIG_URL + '"): parsing of some of the items ' +
             'finished with errors');
   }
 };
@@ -240,19 +237,20 @@ KnownAppsRegistry.prototype.parseJsonAndApply_ = function(json) {
 /**
  * @param {string} key
  * @param {*} value
- * @return {KnownApp?}
+ * @return {TrustedClientInfo?}
  * @private
  */
-KnownAppsRegistry.prototype.tryParseKnownAppJson_ = function(key, value) {
+TrustedClientsRegistry.prototype.tryParseTrustedClientInfo_ = function(
+    key, value) {
   if (!goog.isObject(value))
     return null;
 
-  if (!goog.object.containsKey(value, KNOWN_CLIENT_APP_NAME_FIELD))
+  if (!goog.object.containsKey(value, CLIENT_NAME_FIELD))
     return null;
-  const nameField = value[KNOWN_CLIENT_APP_NAME_FIELD];
+  const nameField = value[CLIENT_NAME_FIELD];
   if (typeof nameField !== 'string')
     return null;
 
-  return new KnownApp(key, nameField);
+  return new TrustedClientInfo(key, nameField);
 };
 });  // goog.scope

smart_card_connector_app/src/window-apps-displaying.js
@@ -24,8 +24,9 @@ goog.provide('GoogleSmartCard.ConnectorApp.Window.AppsDisplaying');
 
 goog.require('GoogleSmartCard.DebugDump');
 goog.require('GoogleSmartCard.Logging');
-goog.require('GoogleSmartCard.PcscLiteServerClientsManagement.PermissionsChecking.KnownApp');
-goog.require('GoogleSmartCard.PcscLiteServerClientsManagement.PermissionsChecking.KnownAppsRegistry');
+goog.require('GoogleSmartCard.MessagingOrigin');
+goog.require('GoogleSmartCard.PcscLiteServer.TrustedClientInfo');
+goog.require('GoogleSmartCard.PcscLiteServer.TrustedClientsRegistry');
 goog.require('goog.Promise');
 goog.require('goog.array');
 goog.require('goog.asserts');
@@ -36,9 +37,7 @@ goog.require('goog.log.Logger');
 goog.scope(function() {
 
 const GSC = GoogleSmartCard;
-const PermissionsChecking =
-    GSC.PcscLiteServerClientsManagement.PermissionsChecking;
-const KnownApp = PermissionsChecking.KnownApp;
+const TrustedClientInfo = GSC.PcscLiteServer.TrustedClientInfo;
 
 /** @type {!goog.log.Logger} */
 const logger = GSC.Logging.getScopedLogger('ConnectorApp.MainWindow');
@@ -47,21 +46,22 @@ const logger = GSC.Logging.getScopedLogger('ConnectorApp.MainWindow');
 const appListElement =
     /** @type {!Element} */ (goog.dom.getElement('app-list'));
 
-const knownAppsRegistry = new PermissionsChecking.KnownAppsRegistry();
+const trustedClientsRegistry = new GSC.PcscLiteServer.TrustedClientsRegistry();
 
 /**
- * @type {goog.Promise.<!Array.<!KnownApp>>?}
+ * @type {goog.Promise.<!Array.<!TrustedClientInfo>>?}
  */
-let lastKnownAppsPromise = null;
+let lastTrustedClientInfosPromise = null;
 
 /**
- * @param {!goog.Promise.<!Array.<!KnownApp>>} knownAppsPromise
+ * @param {!goog.Promise.<!Array.<!TrustedClientInfo>>}
+ *     trustedClientInfosPromise
  * @param {!Array.<string>} appIds
- * @param {Array.<!KnownApp>?} knownApps
+ * @param {Array.<!TrustedClientInfo>?} trustedClientInfos
  * @return {boolean}
  */
-function updateAppView(knownAppsPromise, appIds, knownApps) {
-  if (knownAppsPromise !== lastKnownAppsPromise)
+function updateAppView(trustedClientInfosPromise, appIds, trustedClientInfos) {
+  if (trustedClientInfosPromise !== lastTrustedClientInfosPromise)
     return false;
 
   GSC.Logging.checkWithLogger(logger, appListElement !== null);
@@ -70,8 +70,9 @@ function updateAppView(knownAppsPromise, appIds, knownApps) {
   goog.dom.removeChildren(appListElement);
 
   for (let i = 0; i < appIds.length; i++) {
-    const text =
-        knownApps && knownApps[i] ? knownApps[i].name : '<' + appIds[i] + '>';
+    const text = trustedClientInfos && trustedClientInfos[i] ?
+        trustedClientInfos[i].name :
+        '<' + appIds[i] + '>';
     const newElement = goog.dom.createDom('li', undefined, text);
     goog.dom.append(appListElement, newElement);
   }
@@ -80,28 +81,29 @@ function updateAppView(knownAppsPromise, appIds, knownApps) {
 }
 
 /**
- * @param {!Array.<string>} appListArg
+ * @param {!Array.<string>} appList
  */
-function onUpdateListener(appListArg) {
-  const appList = goog.array.clone(appListArg);
-  goog.array.sort(appList);
+function onUpdateListener(appList) {
+  const originList = appList.map(GSC.MessagingOrigin.getFromExtensionId);
+  goog.array.sort(originList);
   goog.log.fine(
       logger,
       'Application list updated, refreshing the view. ' +
-          'New list of id\'s: ' + GSC.DebugDump.dump(appList));
+          'New list of origins: ' + GSC.DebugDump.dump(originList));
 
-  const knownAppsPromise = knownAppsRegistry.tryGetByIds(appList);
-  lastKnownAppsPromise = knownAppsPromise;
+  const trustedClientInfosPromise =
+      trustedClientsRegistry.tryGetByOrigins(originList);
+  lastTrustedClientInfosPromise = trustedClientInfosPromise;
 
-  knownAppsPromise.then(
-      function(knownApps) {
-        updateAppView(knownAppsPromise, appList, knownApps);
+  trustedClientInfosPromise.then(
+      function(trustedClientInfos) {
+        updateAppView(trustedClientInfosPromise, appList, trustedClientInfos);
       },
       function(error) {
-        if (!updateAppView(knownAppsPromise, appList, null))
+        if (!updateAppView(trustedClientInfosPromise, appList, null))
           return;
 
-        goog.log.warning(logger, 'Couldn\'t resolve appList: ' + error);
+        goog.log.warning(logger, 'Couldn\'t resolve origins: ' + error);
       });
 }
 

third_party/pcsc-lite/naclport/server_clients_management/src/permissions_checking/user-prompting-checker-unittest.js
@@ -15,9 +15,10 @@
  * limitations under the License.
  */
 
+goog.require('GoogleSmartCard.MessagingOrigin');
 goog.require('GoogleSmartCard.ObjectHelpers');
-goog.require('GoogleSmartCard.PcscLiteServerClientsManagement.PermissionsChecking.KnownApp');
-goog.require('GoogleSmartCard.PcscLiteServerClientsManagement.PermissionsChecking.KnownAppsRegistry');
+goog.require('GoogleSmartCard.PcscLiteServer.TrustedClientInfo');
+goog.require('GoogleSmartCard.PcscLiteServer.TrustedClientsRegistry');
 goog.require('GoogleSmartCard.PcscLiteServerClientsManagement.PermissionsChecking.UserPromptingChecker');
 goog.require('GoogleSmartCard.PopupOpener');
 goog.require('goog.Promise');
@@ -33,17 +34,18 @@ goog.scope(function() {
 
 const GSC = GoogleSmartCard;
 
-const PermissionsChecking =
-    GSC.PcscLiteServerClientsManagement.PermissionsChecking;
-const KnownApp = PermissionsChecking.KnownApp;
-const KnownAppsRegistry = PermissionsChecking.KnownAppsRegistry;
-const UserPromptingChecker = PermissionsChecking.UserPromptingChecker;
+const TrustedClientInfo = GSC.PcscLiteServer.TrustedClientInfo;
+const TrustedClientsRegistry = GSC.PcscLiteServer.TrustedClientsRegistry;
+const UserPromptingChecker = GSC.PcscLiteServerClientsManagement
+                                 .PermissionsChecking.UserPromptingChecker;
 const ignoreArgument = goog.testing.mockmatchers.ignoreArgument;
 
-const FAKE_APP_1_ID = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
-const FAKE_APP_1_NAME = 'App Name 1';
-const FAKE_APP_1_KNOWN_APP = new KnownApp(FAKE_APP_1_ID, FAKE_APP_1_NAME);
-const FAKE_APP_2_ID = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb';
+const FAKE_CLIENT_1_ID = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+const FAKE_CLIENT_1_NAME = 'Application 1';
+const FAKE_TRUSTED_CLIENT_INFO_1 = new TrustedClientInfo(
+    GSC.MessagingOrigin.getFromExtensionId(FAKE_CLIENT_1_ID),
+    FAKE_CLIENT_1_NAME);
+const FAKE_CLIENT_2_ID = 'bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb';
 const STORAGE_KEY = 'pcsc_lite_clients_user_selections';
 
 /**
@@ -59,20 +61,20 @@ const MockedDialogBehavior = {
 /**
  * Sets up a mock instance of KnownAppsRegistry.
  *
- * This mock instance is only aware of |FAKE_APP_1_ID|.
+ * This mock instance is only aware of |FAKE_CLIENT_1_ID|.
  * @param {!goog.testing.MockControl} mockControl
  */
 function setUpKnownAppsRegistryMock(mockControl) {
   const mockInstance = {
-    getById: function(id) {
-      if (id == FAKE_APP_1_ID)
-        return goog.Promise.resolve(FAKE_APP_1_KNOWN_APP);
+    getByOrigin: function(origin) {
+      if (origin === GSC.MessagingOrigin.getFromExtensionId(FAKE_CLIENT_1_ID))
+        return goog.Promise.resolve(FAKE_TRUSTED_CLIENT_INFO_1);
       return goog.Promise.reject();
     }
   };
   /** @type {?} */
   const mockedConstructor = mockControl.createConstructorMock(
-      PermissionsChecking, 'KnownAppsRegistry');
+      GSC.PcscLiteServer, 'TrustedClientsRegistry');
   mockedConstructor().$returns(mockInstance);
 }
 
@@ -228,11 +230,11 @@ goog.exportSymbol(
     'test_UserPromptingChecker_EmptyStorage_UserApproves',
     makeTest(
         {} /* fakeInitialStorageData */, {
-          [STORAGE_KEY]: {[FAKE_APP_1_ID]: true}
+          [STORAGE_KEY]: {[FAKE_CLIENT_1_ID]: true}
         } /* expectedStorageDataToBeWritten */,
         MockedDialogBehavior.USER_APPROVES /* mockedDialogBehavior */,
         function(userPromptingChecker) {
-          return userPromptingChecker.check(FAKE_APP_1_ID);
+          return userPromptingChecker.check(FAKE_CLIENT_1_ID);
         }));
 
 goog.exportSymbol(
@@ -242,7 +244,7 @@ goog.exportSymbol(
         null /* expectedStorageDataToBeWritten */,
         MockedDialogBehavior.USER_DENIES /* mockedDialogBehavior */,
         function(userPromptingChecker) {
-          return negatePromise(userPromptingChecker.check(FAKE_APP_1_ID));
+          return negatePromise(userPromptingChecker.check(FAKE_CLIENT_1_ID));
         }));
 
 goog.exportSymbol(
@@ -252,43 +254,46 @@ goog.exportSymbol(
         null /* expectedStorageDataToBeWritten */,
         MockedDialogBehavior.USER_CANCELS /* mockedDialogBehavior */,
         function(userPromptingChecker) {
-          return negatePromise(userPromptingChecker.check(FAKE_APP_1_ID));
+          return negatePromise(userPromptingChecker.check(FAKE_CLIENT_1_ID));
         }));
 
 goog.exportSymbol(
     'test_UserPromptingChecker_AlreadyInStorage_OneItem',
     makeTest(
-        {[STORAGE_KEY]: {[FAKE_APP_1_ID]: true}} /* fakeInitialStorageData */,
+        {[STORAGE_KEY]:
+             {[FAKE_CLIENT_1_ID]: true}} /* fakeInitialStorageData */,
         null /* expectedStorageDataToBeWritten */,
         MockedDialogBehavior.NOT_RUN /* mockedDialogBehavior */,
         function(userPromptingChecker) {
-          return userPromptingChecker.check(FAKE_APP_1_ID);
+          return userPromptingChecker.check(FAKE_CLIENT_1_ID);
         }));
 
 goog.exportSymbol(
     'test_UserPromptingChecker_AlreadyInStorage_TwoItems',
     makeTest(
         {
-          [STORAGE_KEY]: {[FAKE_APP_1_ID]: true, [FAKE_APP_2_ID]: true}
+          [STORAGE_KEY]: {[FAKE_CLIENT_1_ID]: true, [FAKE_CLIENT_2_ID]: true}
         } /* fakeInitialStorageData */,
         null /* expectedStorageDataToBeWritten */,
         MockedDialogBehavior.NOT_RUN /* mockedDialogBehavior */,
         function(userPromptingChecker) {
           return goog.Promise.all([
-            userPromptingChecker.check(FAKE_APP_1_ID),
-            userPromptingChecker.check(FAKE_APP_2_ID)
+            userPromptingChecker.check(FAKE_CLIENT_1_ID),
+            userPromptingChecker.check(FAKE_CLIENT_2_ID)
           ]);
         }));
 
 goog.exportSymbol(
     'test_UserPromptingChecker_OtherInStorage_UserApproves',
     makeTest(
-        {[STORAGE_KEY]: {[FAKE_APP_1_ID]: true}} /* fakeInitialStorageData */, {
-          [STORAGE_KEY]: {[FAKE_APP_1_ID]: true, [FAKE_APP_2_ID]: true}
+        {[STORAGE_KEY]:
+             {[FAKE_CLIENT_1_ID]: true}} /* fakeInitialStorageData */,
+        {
+          [STORAGE_KEY]: {[FAKE_CLIENT_1_ID]: true, [FAKE_CLIENT_2_ID]: true}
         } /* expectedStorageDataToBeWritten */,
         MockedDialogBehavior.USER_APPROVES /* mockedDialogBehavior */,
         function(userPromptingChecker) {
-          return userPromptingChecker.check(FAKE_APP_2_ID);
+          return userPromptingChecker.check(FAKE_CLIENT_2_ID);
         }));
 
 // Regression test for issue #57.
@@ -296,24 +301,26 @@ goog.exportSymbol(
     'test_UserPromptingChecker_CorruptedStorage_NonObject',
     makeTest(
         {[STORAGE_KEY]: 'foo'} /* fakeInitialStorageData */, {
-          [STORAGE_KEY]: {[FAKE_APP_1_ID]: true}
+          [STORAGE_KEY]: {[FAKE_CLIENT_1_ID]: true}
         } /* expectedStorageDataToBeWritten */,
         MockedDialogBehavior.USER_APPROVES /* mockedDialogBehavior */,
         function(userPromptingChecker) {
-          return userPromptingChecker.check(FAKE_APP_1_ID);
+          return userPromptingChecker.check(FAKE_CLIENT_1_ID);
         }));
 
 // Regression test for issue #57.
 goog.exportSymbol(
     'test_UserPromptingChecker_CorruptedStorage_BadItem',
     makeTest(
-        {[STORAGE_KEY]: {[FAKE_APP_1_ID]: 'foo'}} /* fakeInitialStorageData */,
         {
-          [STORAGE_KEY]: {[FAKE_APP_1_ID]: true}
+          [STORAGE_KEY]: {[FAKE_CLIENT_1_ID]: 'foo'}
+        } /* fakeInitialStorageData */,
+        {
+          [STORAGE_KEY]: {[FAKE_CLIENT_1_ID]: true}
         } /* expectedStorageDataToBeWritten */,
         MockedDialogBehavior.USER_APPROVES /* mockedDialogBehavior */,
         function(userPromptingChecker) {
-          return userPromptingChecker.check(FAKE_APP_1_ID);
+          return userPromptingChecker.check(FAKE_CLIENT_1_ID);
         }));
 
 // Regression test for issue #57.
@@ -321,11 +328,11 @@ goog.exportSymbol(
     'test_UserPromptingChecker_CorruptedStorage_BadOtherItem',
     makeTest(
         {
-          [STORAGE_KEY]: {[FAKE_APP_1_ID]: true, [FAKE_APP_2_ID]: 'foo'}
+          [STORAGE_KEY]: {[FAKE_CLIENT_1_ID]: true, [FAKE_CLIENT_2_ID]: 'foo'}
         } /* fakeInitialStorageData */,
         null /* expectedStorageDataToBeWritten */,
         MockedDialogBehavior.NOT_RUN /* mockedDialogBehavior */,
         function(userPromptingChecker) {
-          return userPromptingChecker.check(FAKE_APP_1_ID);
+          return userPromptingChecker.check(FAKE_CLIENT_1_ID);
         }));
 });  // goog.scope

commit 7c9d320f32bb423870ef48ea328d2cbdb4ec23b9

[js] Fix bad "this" in TrustedClientsRegistry
The code in TrustedClientsRegistry made an incorrect dereference of
"this" inside a local function. Fix this by changing the function to be
a lambda (a.k.a. "arrow function"), which inherits "this" from the outer
scope.

The bug was introduced in #412. Not sure why it wasn't visible earlier -
probably the recent changes of JS compilation levels made the compiler
produce slightly different code.

third_party/pcsc-lite/naclport/server_clients_management/src/permissions_checking/trusted-clients-registry.js
@@ -104,7 +104,7 @@ TrustedClientsRegistry.prototype.getByOrigin = function(origin) {
   const promiseResolver = goog.Promise.withResolver();
 
   this.tryGetByOrigins([origin]).then(
-      function(infos) {
+      infos => {
         GSC.Logging.checkWithLogger(this.logger, infos.length === 1);
         const info = infos[0];
         if (info) {