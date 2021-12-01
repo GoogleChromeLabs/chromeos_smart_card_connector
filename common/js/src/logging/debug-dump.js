GoogleChromeLabs/chromeos_smart_card_connector

commit 4f8c34e45cfb97a62c0b564eb67620fefecc5342

[common] Optimize ArrayBuffer debug dumping (#481)
Optimize the JavaScript DebugDump helper that deals with ArrayBuffers
and make the output format more compact (use a single "0x" instead of
prepending "0x" before every byte and using the ", " separator).

The previous implementation was doing lots of overhead by passing every
byte through a bunch of type and boundary checks. All of that is
unnecessary, as we know that after the type casting we're dealing with
an array of unsigned single-byte integers.

We don't have any measurements, but the inefficiency of the old version
could actually be user-visible, since we have a lot of log statements
that are calculated even in the Release mode (unlike when using C/C++,
it's not possible to automatically completely skip log message
calculation in JavaScript, even when we know that we're below the
current log level threshold).

common/js/src/logging/debug-dump.js
@@ -118,6 +118,18 @@ function dumpNumber(value) {
 }
 
 /**
+ * @param {number} value
+ * @return {string}
+ */
+function dumpArrayBufferByte(value) {
+  const HEX_LENGTH_OF_BYTE = 2;
+  const hexValue = value.toString(16).toUpperCase();
+  const zeroPadding =
+      goog.string.repeat('0', HEX_LENGTH_OF_BYTE - hexValue.length);
+  return zeroPadding + hexValue;
+}
+
+/**
  * @param {string} value
  * @return {string}
  */
@@ -146,9 +158,15 @@ function dumpFunction(value) {
  * @return {string}
  */
 function dumpArrayBuffer(value) {
+  if (!value.byteLength)
+    return 'ArrayBuffer[]';
   const bytes = new Uint8Array(value);
-  const dumpedBytes = goog.iter.map(bytes, byte => dumpNumber(byte));
-  return 'ArrayBuffer[' + goog.iter.join(dumpedBytes, ', ') + ']';
+  // The format is different than the one `dump()` produces for arrays, and also
+  // directly calling `dumpArrayBufferByte()` is much faster than calling
+  // generic dump functions.
+  const dumpedBytes = goog.iter.map(bytes, byte => dumpArrayBufferByte(byte));
+  const concatenated = goog.iter.join(dumpedBytes, '');
+  return `ArrayBuffer[0x${concatenated}]`;
 }
 
 /**

common/js/src/logging/debug-dump-unittest.js
@@ -46,8 +46,8 @@ goog.exportSymbol('testDebugDump', function() {
 
   assertEquals('[0x01, "foo", [0x02, 0x03], []]', dump([1, 'foo', [2, 3], []]));
 
-  assertEquals(
-      'ArrayBuffer[0x01, 0xFF]', dump(new Uint8Array([1, 255]).buffer));
+  assertEquals('ArrayBuffer[]', dump(new ArrayBuffer(0)));
+  assertEquals('ArrayBuffer[0x01FF]', dump(new Uint8Array([1, 255]).buffer));
 
   assertEquals(
       'Map{0x01: 0x02, 0x03: "foo", "bar": {}, {}: 0x04}',

commit 192edf5881027d070b1bc330d8218e9ebefabaea

[common] Fix boolean debug dumping in JS
Fix the regression introduced by #481: the dump() function was throwing
this error when called with a boolean:

  TypeError: Invalid value used in weak set

The reason is that WeakSet, used by us in order to detect circular
references, doesn't support non-objects. And also that we also never
handle the boolean value explicitly - it was dumped by the fallback JSON
serialization (which might've been a small performance issue itself).

This commit fixes both aspects: it dumps booleans explicitly, but also
adds a safety fallback for non-objects before triggering WeakSet
operations.

common/js/src/logging/debug-dump.js
@@ -44,6 +44,14 @@ function encodeJson(value) {
 }
 
 /**
+ * @param {boolean} value
+ * @return {string}
+ */
+function dumpBoolean(value) {
+  return value ? 'true' : 'false';
+}
+
+/**
  * @param {number} value
  * @return {number}
  */
@@ -262,18 +270,26 @@ function dump(value, recursionParentObjects) {
     return 'undefined';
   if (value === null)
     return 'null';
-  if (typeof value === 'number')
-    return dumpNumber(value);
-  if (typeof value === 'string')
-    return dumpString(value);
+  switch (typeof value) {
+    case 'boolean':
+      return dumpBoolean(value);
+    case 'number':
+      return dumpNumber(value);
+    case 'string':
+      return dumpString(value);
+  }
   if (goog.functions.isFunction(value))
     return dumpFunction(value);
   if (value instanceof ArrayBuffer)
     return dumpArrayBuffer(value);
 
   // Detect circular references and prevent infinite recursion by checking
-  // against the values in the upper stack frames. Only do this after handling
-  // all non-recursive cases, in order to reduce the performance penalty.
+  // against the values in the upper stack frames.
+  if (!goog.isObject(value)) {
+    // `WeakSet` only supports objects, and normally all non-objects should be
+    // handled above. As a safety measure, exit here too.
+    return encodeJson(value);
+  }
   if (recursionParentObjects.has(value))
     return '<circular>';
   recursionParentObjects.add(value);

common/js/src/logging/debug-dump-unittest.js
@@ -32,6 +32,9 @@ goog.exportSymbol('testDebugDump', function() {
 
   assertEquals('null', dump(null));
 
+  assertEquals('false', dump(false));
+  assertEquals('true', dump(true));
+
   assertEquals('0x00', dump(0));
   assertEquals('0xFE', dump(254));
   assertEquals('0x00000100', dump(256));