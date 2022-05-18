f2be29576793de3d38e6196621f56d9564c9cf36
Maksim Ivanov <emaxx@google.com>
2022-05-18 16:42:49 +0200

    wip

--- env/initialize.sh
+++ env/initialize.sh
@@ -200,7 +200,7 @@ create_activate_script() {
   # scripts still use Python 2.
   log_message "Creating \"activate\" script..."
   echo > activate
-  # echo "export NACL_SDK_ROOT=${NACL_SDK_ROOT}" >> activate
+  echo "export NACL_SDK_ROOT=${NACL_SDK_ROOT}" >> activate
   echo "source ${SCRIPTPATH}/emsdk/emsdk_env.sh" >> activate
   log_message "\"activate\" script was created successfully. Run \"source $(dirname ${0})/activate\" in order to trigger all necessary environment definitions."
 }
