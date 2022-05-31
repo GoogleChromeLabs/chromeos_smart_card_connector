9f7d7d830c3852d407cc1c00afbfecef070148bf
Maksim Ivanov <emaxx@google.com>
2022-05-31 13:30:10 +0200

    comment

--- smart_card_connector_app/src/testing_smart_card_simulation.cc
+++ smart_card_connector_app/src/testing_smart_card_simulation.cc
@@ -42,7 +42,10 @@ TestingSmartCardSimulation::~TestingSmartCardSimulation() = default;
 
 void TestingSmartCardSimulation::OnRequestToJs(Value request_payload,
                                                optional<RequestId> request_id) {
+  // Make the debug dump in advance, before we know whether we need to crash,
+  // because we can't dump the value after std::move()'ing it.
   const std::string payload_debug_dump = DebugDumpValueFull(request_payload);
+
   RemoteCallRequestPayload remote_call =
       ConvertFromValueOrDie<RemoteCallRequestPayload>(
           std::move(request_payload));
