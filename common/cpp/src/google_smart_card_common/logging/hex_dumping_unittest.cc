da0a9be46925567a0dbefede895773374579fd05
Maksim Ivanov <emaxx@google.com>
2022-06-09 15:59:08 +0200

    Unit-test hex dumping C++ helpers (#655)
    Add unit tests for untested helpers in hex_dumping.h in //common/.

--- common/cpp/src/google_smart_card_common/logging/hex_dumping_unittest.cc
+++ common/cpp/src/google_smart_card_common/logging/hex_dumping_unittest.cc
@@ -23,77 +23,112 @@
 namespace google_smart_card {
 
 TEST(LoggingHexDumpingTest, HexDumpByte) {
-  EXPECT_EQ("0x00", HexDumpByte(static_cast<int8_t>(0)));
-  EXPECT_EQ("0x00", HexDumpByte(static_cast<uint8_t>(0)));
+  EXPECT_EQ(HexDumpByte(static_cast<int8_t>(0)), "0x00");
+  EXPECT_EQ(HexDumpByte(static_cast<uint8_t>(0)), "0x00");
 
-  EXPECT_EQ("0x01", HexDumpByte(static_cast<int8_t>(1)));
-  EXPECT_EQ("0x01", HexDumpByte(static_cast<uint8_t>(1)));
+  EXPECT_EQ(HexDumpByte(static_cast<int8_t>(1)), "0x01");
+  EXPECT_EQ(HexDumpByte(static_cast<uint8_t>(1)), "0x01");
 
-  EXPECT_EQ("0x64", HexDumpByte(static_cast<int8_t>(100)));
-  EXPECT_EQ("0x64", HexDumpByte(static_cast<uint8_t>(100)));
+  EXPECT_EQ(HexDumpByte(static_cast<int8_t>(100)), "0x64");
+  EXPECT_EQ(HexDumpByte(static_cast<uint8_t>(100)), "0x64");
 
-  EXPECT_EQ("0xFF", HexDumpByte(static_cast<int8_t>(-1)));
-  EXPECT_EQ("0xFF", HexDumpByte(static_cast<uint8_t>(255)));
+  EXPECT_EQ(HexDumpByte(static_cast<int8_t>(-1)), "0xFF");
+  EXPECT_EQ(HexDumpByte(static_cast<uint8_t>(255)), "0xFF");
+}
+
+TEST(LoggingHexDumpingTest, HexDumpDoublet) {
+  EXPECT_EQ(HexDumpDoublet(static_cast<int16_t>(0)), "0x0000");
+  EXPECT_EQ(HexDumpDoublet(static_cast<uint16_t>(0)), "0x0000");
+
+  EXPECT_EQ(HexDumpDoublet(static_cast<int16_t>(1)), "0x0001");
+  EXPECT_EQ(HexDumpDoublet(static_cast<uint16_t>(1)), "0x0001");
+
+  EXPECT_EQ(HexDumpDoublet(static_cast<int16_t>(-1)), "0xFFFF");
+  EXPECT_EQ(HexDumpDoublet(static_cast<uint16_t>(-1)), "0xFFFF");
 }
 
 TEST(LoggingHexDumpingTest, HexDumpQuadlet) {
-  EXPECT_EQ("0x00000000", HexDumpQuadlet(static_cast<int32_t>(0)));
-  EXPECT_EQ("0x00000000", HexDumpQuadlet(static_cast<uint32_t>(0)));
+  EXPECT_EQ(HexDumpQuadlet(static_cast<int32_t>(0)), "0x00000000");
+  EXPECT_EQ(HexDumpQuadlet(static_cast<uint32_t>(0)), "0x00000000");
 
-  EXPECT_EQ("0xFFFFFFFF", HexDumpQuadlet(static_cast<int32_t>(-1)));
-  EXPECT_EQ("0xFFFFFFFF", HexDumpQuadlet(static_cast<uint32_t>(-1)));
+  EXPECT_EQ(HexDumpQuadlet(static_cast<int32_t>(1)), "0x00000001");
+  EXPECT_EQ(HexDumpQuadlet(static_cast<uint32_t>(1)), "0x00000001");
+
+  EXPECT_EQ(HexDumpQuadlet(static_cast<int32_t>(-1)), "0xFFFFFFFF");
+  EXPECT_EQ(HexDumpQuadlet(static_cast<uint32_t>(-1)), "0xFFFFFFFF");
 }
 
 TEST(LoggingHexDumpingTest, HexDumpOctlet) {
-  EXPECT_EQ("0x0000000000000000", HexDumpOctlet(static_cast<int64_t>(0)));
-  EXPECT_EQ("0x0000000000000000", HexDumpOctlet(static_cast<uint64_t>(0)));
+  EXPECT_EQ(HexDumpOctlet(static_cast<int64_t>(0)), "0x0000000000000000");
+  EXPECT_EQ(HexDumpOctlet(static_cast<uint64_t>(0)), "0x0000000000000000");
 
-  EXPECT_EQ("0xFFFFFFFFFFFFFFFF", HexDumpOctlet(static_cast<int64_t>(-1)));
-  EXPECT_EQ("0xFFFFFFFFFFFFFFFF", HexDumpOctlet(static_cast<uint64_t>(-1)));
+  EXPECT_EQ(HexDumpOctlet(static_cast<int64_t>(1)), "0x0000000000000001");
+  EXPECT_EQ(HexDumpOctlet(static_cast<uint64_t>(1)), "0x0000000000000001");
+
+  EXPECT_EQ(HexDumpOctlet(static_cast<int64_t>(-1)), "0xFFFFFFFFFFFFFFFF");
+  EXPECT_EQ(HexDumpOctlet(static_cast<uint64_t>(-1)), "0xFFFFFFFFFFFFFFFF");
 }
 
 TEST(LoggingHexDumpingTest, HexDumpInteger) {
-  EXPECT_EQ("0x00", HexDumpInteger(static_cast<int8_t>(0)));
-  EXPECT_EQ("0x00", HexDumpInteger(static_cast<uint8_t>(0)));
+  EXPECT_EQ(HexDumpInteger(static_cast<int8_t>(0)), "0x00");
+  EXPECT_EQ(HexDumpInteger(static_cast<int8_t>(12)), "0x0C");
+  EXPECT_EQ(HexDumpInteger(static_cast<uint8_t>(0)), "0x00");
+  EXPECT_EQ(HexDumpInteger(static_cast<uint8_t>(34)), "0x22");
 
-  EXPECT_EQ("0x00000000", HexDumpInteger(static_cast<int32_t>(0)));
-  EXPECT_EQ("0x00000000", HexDumpInteger(static_cast<uint32_t>(0)));
+  EXPECT_EQ(HexDumpInteger(static_cast<int32_t>(0)), "0x00000000");
+  EXPECT_EQ(HexDumpInteger(static_cast<int32_t>(1234)), "0x000004D2");
+  EXPECT_EQ(HexDumpInteger(static_cast<uint32_t>(0)), "0x00000000");
+  EXPECT_EQ(HexDumpInteger(static_cast<uint32_t>(4567)), "0x000011D7");
 
-  EXPECT_EQ("0x0000000000000000", HexDumpInteger(static_cast<int64_t>(0)));
-  EXPECT_EQ("0x0000000000000000", HexDumpInteger(static_cast<uint64_t>(0)));
+  EXPECT_EQ(HexDumpInteger(static_cast<int64_t>(0)), "0x0000000000000000");
+  EXPECT_EQ(HexDumpInteger(static_cast<int64_t>(123)), "0x000000000000007B");
+  EXPECT_EQ(HexDumpInteger(static_cast<uint64_t>(0)), "0x0000000000000000");
+  EXPECT_EQ(HexDumpInteger(static_cast<uint64_t>(456)), "0x00000000000001C8");
 }
 
 TEST(LoggingHexDumpingTest, HexDumpUnknownSizeInteger) {
-  EXPECT_EQ("0x00", HexDumpUnknownSizeInteger(static_cast<int64_t>(0)));
-  EXPECT_EQ("0x00", HexDumpUnknownSizeInteger(static_cast<uint64_t>(0)));
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<int64_t>(0)), "0x00");
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<uint64_t>(0)), "0x00");
 
-  EXPECT_EQ("0xFF", HexDumpUnknownSizeInteger(static_cast<int64_t>(255)));
-  EXPECT_EQ("0xFF", HexDumpUnknownSizeInteger(static_cast<uint64_t>(255)));
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<int64_t>(255)), "0xFF");
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<uint64_t>(255)), "0xFF");
 
-  EXPECT_EQ("0x00000100", HexDumpUnknownSizeInteger(static_cast<int64_t>(256)));
-  EXPECT_EQ("0x00000100",
-            HexDumpUnknownSizeInteger(static_cast<uint64_t>(256)));
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<int64_t>(256)), "0x00000100");
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<uint64_t>(256)),
+            "0x00000100");
 
-  EXPECT_EQ("0xFFFFFFFF",
-            HexDumpUnknownSizeInteger(static_cast<int64_t>((1LL << 32) - 1)));
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<int64_t>((1LL << 32) - 1)),
+            "0xFFFFFFFF");
   EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<uint64_t>((1LL << 32) - 1)),
             "0xFFFFFFFF");
 
-  EXPECT_EQ("0x0000000100000000",
-            HexDumpUnknownSizeInteger(static_cast<int64_t>(1LL << 32)));
-  EXPECT_EQ("0x0000000100000000",
-            HexDumpUnknownSizeInteger(static_cast<uint64_t>(1LL << 32)));
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<int64_t>(1LL << 32)),
+            "0x0000000100000000");
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<uint64_t>(1LL << 32)),
+            "0x0000000100000000");
 
-  EXPECT_EQ("0x7FFFFFFFFFFFFFFF",
-            HexDumpUnknownSizeInteger(std::numeric_limits<int64_t>::max()));
-  EXPECT_EQ("0xFFFFFFFFFFFFFFFF",
-            HexDumpUnknownSizeInteger(static_cast<uint64_t>(-1)));
+  EXPECT_EQ(HexDumpUnknownSizeInteger(std::numeric_limits<int64_t>::max()),
+            "0x7FFFFFFFFFFFFFFF");
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<uint64_t>(-1)),
+            "0xFFFFFFFFFFFFFFFF");
 
-  EXPECT_EQ("0xFF", HexDumpUnknownSizeInteger(static_cast<int64_t>(-1)));
-  EXPECT_EQ("0xFFFFFF00",
-            HexDumpUnknownSizeInteger(static_cast<int64_t>(-256)));
-  EXPECT_EQ("0x8000000000000000",
-            HexDumpUnknownSizeInteger(std::numeric_limits<int64_t>::min()));
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<int64_t>(-1)), "0xFF");
+  EXPECT_EQ(HexDumpUnknownSizeInteger(static_cast<int64_t>(-256)),
+            "0xFFFFFF00");
+  EXPECT_EQ(HexDumpUnknownSizeInteger(std::numeric_limits<int64_t>::min()),
+            "0x8000000000000000");
+}
+
+TEST(LoggingHexDumpingTest, HexDumpBytes) {
+  EXPECT_EQ(HexDumpBytes(nullptr, 0), "");
+
+  const uint8_t kArray[] = {1, 2, 123};
+  EXPECT_EQ(HexDumpBytes(kArray, 3), "0x01 0x02 0x7B");
+}
+
+TEST(LoggingHexDumpingTest, HexDumpBytesVector) {
+  EXPECT_EQ(HexDumpBytes({}), "");
+  EXPECT_EQ(HexDumpBytes({1, 2, 123}), "0x01 0x02 0x7B");
 }
 
 }  // namespace google_smart_card
