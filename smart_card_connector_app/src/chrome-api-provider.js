593095f275f85a00aae2c52f29a849eb70780171
Viktoriia Kovalova <vkovalova@google.com>
2023-05-17 13:48:03 +0000

    use constants from api.js

--- smart_card_connector_app/src/chrome-api-provider.js
+++ smart_card_connector_app/src/chrome-api-provider.js
@@ -1,5 +1,23 @@
+/**
+ * @license
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 goog.provide('GoogleSmartCard.ConnectorApp.ChromeApiProvider');
 
+goog.require('GoogleSmartCard.PcscLiteClient.API');
 goog.require('GoogleSmartCard.PcscLiteServerClientsManagement.ServerRequestHandler');
 goog.require('goog.Disposable');
 goog.require('goog.log');
@@ -10,9 +28,27 @@ goog.scope(function() {
 
 const GSC = GoogleSmartCard;
 const Pcsc = GSC.PcscLiteServerClientsManagement;
+const PcscApi = GSC.PcscLiteClient.API;
 
 /** @type {!goog.log.Logger} */
 const logger = GSC.Logging.getScopedLogger('ConnectorApp.ChromeApiProvider');
+
+/**
+ * Returns the result code for chrome.smartCardProviderPrivate API that
+ * corresponds to the given PCSC error.
+ * @param {!PcscApi.ERROR_CODE} errorCode
+ * @return {!chrome.smartCardProviderPrivate.ResultCode}
+ * @throws {Error} Will throw if unknowm error code encountered.
+ */
+function convertErrorCodeToEnum(errorCode) {
+  switch (errorCode) {
+    case PcscApi.SCARD_F_INTERNAL_ERROR:
+      return chrome.smartCardProviderPrivate.ResultCode.INTERNAL_ERROR;
+    // TODO(vkovalova): add other error codes
+    default:
+      throw new Error('Unknown error code ' + errorCode + ' encountered');
+  }
+}
 
 /**
  * This class provides chrome.smartCardProviderPrivate API with PCSC responses.
@@ -35,50 +71,88 @@ GSC.ConnectorApp.ChromeApiProvider = class extends goog.Disposable {
 
     this.subscribeToChromeApiEvents_();
 
-    // TODO: addOnDisposeCallback
+    this.serverRequester_.addOnDisposeCallback(this.disposeInternal.bind(this));
   }
 
   /** @override */
   disposeInternal() {
-    // TODO: add impl
+    this.unsubscribeFromChromeApiEvents_();
+    this.serverRequester_.dispose()
+    super.disposeInternal()
   }
 
-  subscribeToChromeApiEvents_() {
+  /**
+   * Checks if chrome.smartCardProviderPrivate is available. Logs if not.
+   * @returns {boolean}
+   * @private
+   */
+  checkChromeAPIAvailable_() {
     if (chrome.smartCardProviderPrivate === undefined) {
-      goog.log.error(
-          logger,
-          'Failed to subscribe to chrome.smartCardProviderPrivate API' +
-              'events as API is not available');
+      goog.log.warning(
+          logger, 'chrome.smartCardProviderPrivate API is not available');
+      return false;
+    }
+    return true;
+  }
+
+  /**
+   * Subscribe to chrome.smartCardProviderPrivate events.
+   * @private
+   */
+  subscribeToChromeApiEvents_() {
+    if (!this.checkChromeAPIAvailable_) {
       return;
     }
 
     chrome.smartCardProviderPrivate.onEstablishContextRequested.addListener(
-        this.webApiEstablishContextListener_.bind(this));
+        this.establishContextListener_.bind(this));
+  }
+  /**
+   * Unsubscribe from chrome.smartCardProviderPrivate events.
+   * @private
+   */
+  unsubscribeFromChromeApiEvents_() {
+    if (!this.checkChromeAPIAvailable_) {
+      return;
+    }
+
+    chrome.smartCardProviderPrivate.onEstablishContextRequested.removeListener(
+        this.establishContextListener_.bind(this));
   }
 
   /**
    * @param {number} requestId
+   * @private
    */
-  async webApiEstablishContextListener_(requestId) {
-    const callArguments = [2, null, null];
+  async establishContextListener_(requestId) {
+    const callArguments = [PcscApi.SCARD_SCOPE_SYSTEM, null, null];
     const remoteCallMessage =
         new GSC.RemoteCallMessage('SCardEstablishContext', callArguments);
-    // console.log("sending request", callArguments, serverRequestHandler);
-    // console.log("webapi: SCardEstablishContext()");
+
     var resultCode = chrome.smartCardProviderPrivate.ResultCode.INTERNAL_ERROR;
     var sCardContext = 0;
+
     try {
-      const result =
+      const responseItems =
           await this.serverRequester_.handleRequest(remoteCallMessage);
 
-      // TODO: map result codes to enum vals.
-      if (result[0] === 0) {
-        resultCode = chrome.smartCardProviderPrivate.ResultCode.SUCCESS;
-      }
-      sCardContext = result[1];
+      const result = new PcscApi.SCardEstablishContextResult(responseItems);
+      result.get(
+          (context) => {
+            sCardContext = context;
+            resultCode = chrome.smartCardProviderPrivate.ResultCode.SUCCESS;
+          },
+          (errorCode) => {
+            resultCode = convertErrorCodeToEnum(errorCode);
+          });
     } catch (error) {
-      // TODO: log error
+      goog.log.log(
+          logger,
+          this.isDisposed() ? goog.log.Level.FINE : goog.log.Level.WARNING,
+          'Failed to process onEstablishContextRequested event' +
+              'with request id ' + requestId + ': ' + error);
     }
+
     chrome.smartCardProviderPrivate.reportEstablishContextResult(
         requestId, sCardContext, resultCode);
   }
