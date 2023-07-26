f5326113e1abc110a1209b79bb47d2190a7bc1cc
Viktoriia Kovalova <vkovalova@google.com>
2023-07-26 18:10:16 +0000

    create a const

--- smart_card_connector_app/src/chrome-api-provider.js
+++ smart_card_connector_app/src/chrome-api-provider.js
@@ -33,6 +33,9 @@ const PcscApi = GSC.PcscLiteClient.API;
 
 /** @type {!goog.log.Logger} */
 const logger = GSC.Logging.getScopedLogger('ConnectorApp.ChromeApiProvider');
+/** @type {number} Bitmask of the lower 16 bits. */
+const lowerBits = 0x0000FFFF;
+
 
 /**
  * Returns the result code for chrome.smartCardProviderPrivate API that
@@ -160,7 +163,7 @@ function convertReaderStateOut(readerState) {
   let readerStateFlags = {};
   // Reader state flags are stored in the lower 16 bits of the event state.
   /** @type {number} */
-  let eventState = readerState['event_state'] & 0x0000FFFF;
+  let eventState = readerState['event_state'] & lowerBits;
   if (eventState == PcscApi.SCARD_STATE_UNAWARE)
     readerStateFlags.unaware = true;
   if (eventState & PcscApi.SCARD_STATE_IGNORE)
@@ -315,7 +318,7 @@ function logUnexpectedConnectionState(state) {
 function convertConnectionStateToEnum(pcscState) {
   // Upper 16 bits contains the number of events for the reader and don't have
   // state flags.
-  state = pcscState & 0x0000FFFF;
+  state = pcscState & lowerBits;
   if (state & PcscApi.SCARD_SPECIFIC) {
     if (state !==
         (PcscApi.SCARD_SPECIFIC | PcscApi.SCARD_POWERED |
