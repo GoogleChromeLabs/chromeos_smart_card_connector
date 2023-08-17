d37fe3e1b4cd6a19a30bfcce933e76a504cb26ac
Viktoriia Kovalova <51924886+ViktoriiaKovalova@users.noreply.github.com>
2023-08-17 19:22:43 +0200

    Add EstablishContext test to chrome-api-jstocxxtest.js (#920)

--- smart_card_connector_app/src/chrome-api-jstocxxtest.js
+++ smart_card_connector_app/src/chrome-api-jstocxxtest.js
@@ -25,6 +25,7 @@ goog.require('GoogleSmartCard.PcscLiteServerClientsManagement.ReadinessTracker')
 goog.require('goog.testing.MockControl');
 goog.require('goog.testing.asserts');
 goog.require('goog.testing.jsunit');
+goog.require('goog.testing.mockmatchers');
 
 goog.setTestOnly();
 
@@ -97,6 +98,24 @@ goog.exportSymbol('testChromeApiProviderToCpp', {
         testController.executableModule.getMessageChannel(),
         pcscReadinessTracker.promise);
     await pcscReadinessTracker.promise;
+  },
+
+  'testEstablishContext': async function() {
+    chrome
+        .smartCardProviderPrivate['reportEstablishContextResult'](
+            /*requestId=*/ 123,
+            /*scardContext=*/ goog.testing.mockmatchers.isNumber, 'SUCCESS')
+        .$once();
+    mockControl.$replayAll();
+
+    launchPcscServer(/*initialDevices=*/[]);
+    chromeApiProvider = new ChromeApiProvider(
+        testController.executableModule.getMessageChannel(),
+        pcscReadinessTracker.promise);
+    mockChromeApi.dispatchEvent(
+        'onEstablishContextRequested', /*requestId=*/ 123);
+    await chrome.smartCardProviderPrivate['reportEstablishContextResult']
+        .$waitAndVerify();
   }
 });
 });  // goog.scope
