dd2f44787a1b6792278f7d9737becf0841757ecf
Maksim Ivanov <emaxx@google.com>
2023-08-17 11:14:51 +0000

    [wasm] Drop support for ancient Chrome (#915)
    Skip generating Emscripten support code for Chrome versions <96. There
    are multiple technical reasons why we won't ship WebAssembly builds to
    such ancient client versions, so let's benefit from it by letting
    Emscripten prune workarounds for them.
    
    Note: the binary size reduction is almost negligible in practice.

--- common/make/internal/executable_building_emscripten.mk
+++ common/make/internal/executable_building_emscripten.mk
@@ -97,6 +97,10 @@ EMSCRIPTEN_COMPILER_FLAGS := \
 # EXPORT_NAME: Name of the JavaScript function that loads the Emscripten module.
 # INCOMING_MODULE_JS_API: Names of Emscripten Module attributes that we need at
 #   runtime (code for other attributes is optimized away).
+# MIN_CHROME_VERSION: Skip generating Emscripten support code for very old
+#   Chrome versions. (The exact boundary is chosen similarly to
+#   minimum_chrome_version in
+#   smart_card_connector_app/src/manifest.json.template.)
 # MIN_*_VERSION: Disable support code for non-Chrome browsers as we're never
 #   expected to run anywhere besides Chrome.
 # MODULARIZE: Puts Emscripten module JavaScript loading code into a factory
@@ -115,6 +119,7 @@ EMSCRIPTEN_LINKER_FLAGS := \
   -s ENVIRONMENT=web,worker \
   -s 'EXPORT_NAME="loadEmscriptenModule_$(TARGET)"' \
   -s INCOMING_MODULE_JS_API=[buffer,instantiateWasm,onAbort,preRun,print,printErr,wasmMemory] \
+  -s MIN_CHROME_VERSION=96 \
   -s MIN_EDGE_VERSION=-1 \
   -s MIN_FIREFOX_VERSION=-1 \
   -s MIN_IE_VERSION=-1 \
