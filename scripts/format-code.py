7573f19a196e8fe6c792de12308252730dde6c0b
Maksim Ivanov <emaxx@google.com>
2023-09-08 11:22:20 +0000

    [style] Add --check-only to format-code script
    Add new parameter "--check-only" to the format-code.py script.
    
    It's primarily intended to be used in CI actions, for checking for
    improperly-formatted code.

--- scripts/format-code.py
+++ scripts/format-code.py
@@ -28,6 +28,7 @@ FORMAT_OPTIONS = [
   # the headers maintained in this project.
   '--sort-includes=false',
 ]
+CHECK_ONLY_OPTIONS = ['--dry-run', '-Werror']
 
 def parse_command_line_args():
   parser = argparse.ArgumentParser(
@@ -36,6 +37,15 @@ def parse_command_line_args():
       '--base', type=str, default='main',
       help='Git ref to diff against, or "none" if the whole repository is to '
            'be reformatted (default: %(default)s)')
+  parser.add_argument(
+      '--check-only', action='store_true',
+      help='When specified, files are only checked for formatting, but no '
+           'edits are made automatically. Non-zero exit code will indicate at '
+           'least one formatting issue.')
+  parser.add_argument(
+      '--llvm-version', type=int,
+      help='LLVM version to use, i.e., "10" to use "clang-format-10". If '
+           'not specified, unqualified "clang-format" is used.')
   return parser.parse_args()
 
 def get_file_paths(args):
@@ -46,7 +56,13 @@ def get_file_paths(args):
           in subprocess.check_output(command).splitlines()]
 
 def run_clang_format(path, args):
-  command = ['clang-format', '-i'] + FORMAT_OPTIONS + [path]
+  program = (f'clang-format-{args.llvm_version}' if args.llvm_version else
+             'clang-format')
+  command = [program, '-i'] + FORMAT_OPTIONS
+  if args.check_only:
+    command += CHECK_ONLY_OPTIONS
+  command += [path]
+  print(f'Running {program}...')
   return subprocess.call(command) == 0
 
 def main():
