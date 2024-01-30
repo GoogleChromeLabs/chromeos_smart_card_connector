GoogleChromeLabs/chromeos_smart_card_connector

commit 8363a91a990298e66e8c18d018993557f6ee3fca

[js] Fix runModalDialog signature (#518)
The runModalDialog function in popup-opener doesn't receive
chrome.app.window.CreateWindowOptions anymore - it's WindowOptions since
the recent refactoring performed in #404.

common/js/src/popup-window/popup-opener.js
@@ -125,25 +125,13 @@ GSC.PopupOpener.createWindow = function(url, windowOptions, opt_data) {
 /**
  * Creates a new modal dialog and returns a promise of the data it returns.
  * @param {string} url
- * @param {!chrome.app.window.CreateWindowOptions=}
- * opt_createWindowOptionsOverrides Overrides to the default window options.
- * Note that the 'id' option is disallowed.
+ * @param {!WindowOptions=} opt_windowOptions
  * @param {!Object=} opt_data Optional data to be passed to the created dialog.
  * @return {!goog.Promise}
  */
-GSC.PopupOpener.runModalDialog = function(
-    url, opt_createWindowOptionsOverrides, opt_data) {
+GSC.PopupOpener.runModalDialog = function(url, opt_windowOptions, opt_data) {
   const createWindowOptions =
       goog.object.clone(DEFAULT_DIALOG_CREATE_WINDOW_OPTIONS);
-
-  if (opt_createWindowOptionsOverrides) {
-    GSC.Logging.checkWithLogger(
-        logger,
-        !goog.object.containsKey(opt_createWindowOptionsOverrides, 'id'),
-        '"id" window option is disallowed for the modal dialogs (as this ' +
-            'option may result in not creating the new modal dialog)');
-    Object.assign(createWindowOptions, opt_createWindowOptionsOverrides);
-  }
 
   if (GSC.Packaging.MODE === GSC.Packaging.Mode.EXTENSION)
     lastUsedPopupId++;

commit 4b1dd391818958d300c203b03cc1f5bcc05e9012

[ui] Fix options passing in runModalDialog
Fix the PopupOpener.runModalDialog() function to correctly pass
the optional window parameters.

They were ignored (this regressed in #518). One noticeable example
was incorrect width of the PIN prompt in the example applications.

common/js/src/popup-window/popup-opener.js
@@ -134,6 +134,8 @@ GSC.PopupOpener.createWindow = function(url, windowOptions, opt_data) {
 GSC.PopupOpener.runModalDialog = function(url, opt_windowOptions, opt_data) {
   const createWindowOptions =
       goog.object.clone(DEFAULT_DIALOG_CREATE_WINDOW_OPTIONS);
+  if (opt_windowOptions !== undefined)
+    Object.assign(createWindowOptions, opt_windowOptions);
 
   if (GSC.Packaging.MODE === GSC.Packaging.Mode.EXTENSION)
     lastUsedPopupId++;