7cf977f41aff1567d251c12bf5938a668993c71f
Maksim Ivanov <emaxx@google.com>
2024-02-06 16:57:27 +0000

    [mv3] Use globalThis instead of window
    Switch a couple of places that use the "window" object to use
    "globalThis" instead. The latter is a better equivalent because
    it works in Manifest V3 Service Workers as well (in a Service
    Worker, "self" should be used instead of "window"; "globalThis" is
    an alias to either of those).

--- common/js/src/random.js
+++ common/js/src/random.js
@@ -36,12 +36,7 @@ const GSC = GoogleSmartCard;
  */
 GSC.Random.randomIntegerNumber = function() {
   const randomBytes = new Uint8Array(RANDOM_INTEGER_BYTE_COUNT);
-  if (typeof window !== 'undefined') {
-    window.crypto.getRandomValues(randomBytes);
-  } else {
-    // We're likely inside a Service Worker.
-    self.crypto.getRandomValues(randomBytes);
-  }
+  globalThis.crypto.getRandomValues(randomBytes);
   let result = 0;
   goog.array.forEach(randomBytes, function(byteValue) {
     result = result * 256 + byteValue;
