f001258d71f3e6f9872d0bbd4a129cf06b02dbc0
Maksim Ivanov <emaxx@google.com>
2024-10-24 21:17:11 +0000

    [pcsc,ccid] Pass install paths from makefiles
    Make single central constants for the PC/SC-Lite's driver installation
    directory and for the CCID config/.so installation paths.
    
    This refactoring gets rid of multiple places in which these constants
    were previously hardcoded in various variations.

--- third_party/ccid/webport/include.mk
+++ third_party/ccid/webport/include.mk
@@ -18,8 +18,9 @@
 #
 # This file contains helper definitions for using this library.
 #
-# /common/make/common.mk and /common/make/executable_building.mk must be
-# included before including this file.
+# /common/make/common.mk, /common/make/executable_building.mk and
+# /third_party/pcsc-lite/naclport/common/include.mk must be included before
+# including this file.
 #
 
 
@@ -27,6 +28,35 @@ CCID_VERSION := 1.5.5
 
 
 CCID_DIR_PATH := $(THIRD_PARTY_DIR_PATH)/ccid
+
+
+# File name of the CCID driver's bundle directory.
+#
+# It could be an arbitrary string, but we just follow what CCID uses on other
+# platforms.
+CCID_BUNDLE_NAME := ifd-ccid.bundle
+
+# File name of the CCID driver's .so file.
+#
+# This value must match the one specified in the "CFBundleExecutable" parameter
+# of the Info.plist config.
+#
+# Note that we don't really create this file in our webport, since all drivers
+# are linked statically, however it's still used by the code to distinguish
+# between calls made to different drivers.
+CCID_SO_NAME := libccid.so
+
+# Path where the CCID driver's config file is to be installed.
+CCID_CONFIG_INSTALLATION_PATH := \
+	$(PCSC_LITE_DRIVER_INSTALLATION_PATH)/$(CCID_BUNDLE_NAME)/Contents/Info.plist
+
+# Path where the CCID driver's .so file is expected to be installed.
+#
+# Note that the .so file doesn't really exist in our webport, since all drivers
+# are linked statically, however it's still used by the code to distinguish
+# between calls made to different drivers.
+CCID_SO_INSTALLATION_PATH := \
+	$(PCSC_LITE_DRIVER_INSTALLATION_PATH)/$(CCID_BUNDLE_NAME)/Contents/$(PCSC_LITE_ARCHITECTURE)/$(CCID_SO_NAME)
 
 
 #
