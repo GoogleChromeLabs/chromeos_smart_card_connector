d7a099ad032fd5043e31e7176ef15e9f6fc3aa94
Maksim Ivanov <emaxx@google.com>
2024-10-29 23:02:56 +0000

    [nacl] Delete SDK and webports (#1209)

--- third_party/webports/src/src/ports/glibc-compat/src/in6_addr.c
+++
@@ -1,6 +0,0 @@
-#include <netinet/in.h>
-
-const struct in6_addr in6addr_any =
-{ { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 } } };
-const struct in6_addr in6addr_loopback =
-{ { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1 } } };
